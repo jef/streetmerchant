00ede13501082f530ea672a349816be1d31621a8
fuckingrobot <fuckingrobot@users.noreply.github.com>
2020-09-18 14:31:55 -0400

    feat: update to check if FE in-stock via Digital River (#29)
    Co-authored-by: Jef LeCompte <jeffreylec@gmail.com>

--- src/store/nvidia.ts
+++ src/store/nvidia.ts
@@ -6,14 +6,8 @@ export const Nvidia: Store = {
 		{
 			brand: 'nvidia',
 			model: 'founders edition',
-			url: 'https://www.nvidia.com/en-us/geforce/buy/',
-			oosLabels: ['out of stock']
-		},
-		{
-			brand: 'nvidia',
-			model: 'founders edition',
-			url: 'https://www.nvidia.com/en-us/shop/geforce/?page=1&limit=9&locale=en-us&search=3080',
-			oosLabels: ['out of stock']
+			url: 'https://api.digitalriver.com/v1/shoppers/me/products/5438481700/inventory-status?apiKey=9485fa7b159e42edb08a83bde0d83dia',
+			oosLabels: ['product_inventory_out_of_stock']
 		}
 	],
 	name: 'nvidia'
