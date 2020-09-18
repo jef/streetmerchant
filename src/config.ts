2255e572d6ac9177d60f4117676a9c0dea3750dc
Ion Caza <contact@johncaza.com>
2020-09-18 10:51:30 -0400

    Makes timeout an environment variable with a default, adds NS_ prefix to current env variables

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,8 @@ import {config} from 'dotenv';
 config({path: resolve(__dirname, '../.env')});
 
 const email = {
-	username: process.env.EMAIL_USERNAME,
-	password: process.env.EMAIL_PASSWORD
+	username: process.env.NS_EMAIL_USERNAME,
+	password: process.env.NS_EMAIL_PASSWORD
 };
 
 const notifications = {
@@ -16,8 +16,14 @@ const page = {
 	height: 1920,
 	userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
 	width: 1080,
-	navigationTimeout: 60000
+	navigationTimeout: 0
 };
+
+if (process.env.NS_TIMEOUT) {
+	page.navigationTimeout = Number(process.env.NS_TIMEOUT);
+} else {
+	page.navigationTimeout = 0; // Change default Navigation Timeout here or set the environment variable
+}
 
 const stores = process.env.STORES ?? 'nvidia';
 
