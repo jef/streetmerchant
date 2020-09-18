d969ebf8849a91f91973ba8fe6364410826211c2
geman220 <geman220@gmail.com>
2020-09-18 10:10:10 -0700

    Create amazon.ts
    amazon store file

---
+++ src/store/amazon.ts
@@ -0,0 +1,56 @@
+import {Store} from './store';
+
+export const amazon: Store = {
+	cartUrl: '',
+	links: [
+		{
+			brand: 'PNY',
+			model: 'XLR8',
+			url: 'https://www.amazon.com/PNY-GeForce-Gaming-Epic-X-Graphics/dp/B08HBR7QBM?ref_=ast_sto_dp',
+			oosLabels: ['currently unavailable']
+		},
+		{
+			brand: 'MSI',
+			model: 'Gaming Trio',
+			url: 'https://www.amazon.com/MSI-GeForce-RTX-3080-10G/dp/B08HR7SV3M?ref_=ast_sto_dp',
+			oosLabels: ['currently unavailable']
+		},
+		{
+			brand: 'EVGA',
+			model: 'FTW3',
+			url: 'https://www.amazon.com/EVGA-10G-P5-3897-KR-GeForce-Technology-Backplate/dp/B08HR3Y5GQ?ref_=ast_sto_dp',
+			oosLabels: ['currently unavailable']
+		},
+		{
+			brand: 'EVGA',
+			model: 'XC3',
+			url: 'https://www.amazon.com/EVGA-10G-P5-3885-KR-GeForce-Cooling-Backplate/dp/B08HR55YB5?ref_=ast_sto_dp',
+			oosLabels: ['currently unavailable']
+		},
+		{
+			brand: 'GIGABYTE',
+			model: 'Windforce',
+			url: 'https://www.amazon.com/GIGABYTE-GeForce-Graphics-WINDFORCE-GV-N3080GAMING/dp/B08HJTH61J?ref_=ast_sto_dp',
+			oosLabels: ['currently unavailable']
+		},
+		{
+			brand: 'GIGABYTE',
+			model: 'Windforce Eagle',
+			url: 'https://www.amazon.com/GIGABYTE-GeForce-Graphics-WINDFORCE-GV-N3080EAGLE/dp/B08HJS2JLJ?ref_=ast_sto_dp',
+			oosLabels: ['currently unavailable']
+		},
+		{
+			brand: 'ASUS',
+			model: 'TUF',
+			url: 'https://www.amazon.com/ASUS-Graphics-DisplayPort-Military-Grade-Certification/dp/B08HH5WF97?ref_=ast_sto_dp',
+			oosLabels: ['currently unavailable']
+		},
+		{
+			brand: 'MSI',
+			model: 'Ventus',
+			url: 'https://www.amazon.com/MSI-GeForce-RTX-3080-10G/dp/B08HR5SXPS?ref_=ast_sto_dp',
+			oosLabels: ['currently unavailable']
+		}
+	],
+	name: 'amazon'
+};
