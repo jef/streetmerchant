3e6b9832fc0b0b7485d58df0a2b7546bf7325e5b
malbert69 <m_albert@hotmail.com>
2020-09-18 14:51:15 -0500

    Create microcenter.ts
    Microcenter store file with all it's 3080 cards.

---
+++ src/store/microcenter.ts
@@ -0,0 +1,44 @@
+import {Store} from './store';
+
+export const MicroCenter: Store = {
+	cartUrl: '',
+	links: [
+		{
+			brand: 'evga',
+			model: 'xc3 ultra gaming',
+			url: 'https://www.microcenter.com/product/628344/evga-geforce-rtx-3080-xc3-ultra-gaming-triple-fan-10gb-gddr6x-pcie-40-graphics-card',
+			oosLabels: ['sold out']
+		},
+	    {
+	        brand: 'msi',
+	        model: 'ventus 3x overclocked',
+	        url: 'https://www.microcenter.com/product/628331/msi-geforce-rtx-3080-ventus-3x-overclocked-triple-fan-10gb-gddr6x-pcie-40-graphics-card',
+	        oosLabels: ['sold out']
+	    },
+	    {
+	        brand: 'asus',
+	        model: 'tuf gaming',
+	        url: 'https://www.microcenter.com/product/628303/asus-geforce-rtx-3080-tuf-gaming-triple-fan-10gb-gddr6x-pcie-40-graphics-card',
+	        oosLabels: ['sold out']
+	    },
+	    {
+	        brand: 'msi',
+	        model: 'gaming x trio',
+	        url: 'https://www.microcenter.com/product/628330/msi-geforce-rtx-3080-gaming-x-trio-triple-fan-10gb-gddr6x-pcie-40-graphics-card',
+	        oosLabels: ['sold out']
+	    },
+	    {
+	        brand: 'evga',
+	        model: 'xc3 black',
+	        url: 'https://www.microcenter.com/product/628340/evga-geforce-rtx-3080-xc3-black-triple-fan-10gb-gddr6x-pcie-40-graphics-card',
+	        oosLabels: ['sold out']
+	    },
+	    {
+	        brand: 'zotac',
+	        model: 'trinity overclocked',
+	        url: 'https://www.microcenter.com/product/628607/zotac-geforce-rtx-3080-trinity-overclocked-triple-fan-10gb-gddr6x-pcie-40-graphics-card',
+	        oosLabels: ['sold out']
+	    }
+	],
+	name: 'microcenter'
+};
