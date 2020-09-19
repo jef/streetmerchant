33ca9b4c28dde2ec496cb2dc94b0cce8b28aae01
Kevin Tong <kcr1230@hotmail.com>
2020-09-18 22:30:22 -0700

    Fix XC3 Ultra Link
    Link directed to ROG STRIX instead of XC3 Ultra

--- src/store/bestbuy.ts
+++ src/store/bestbuy.ts
@@ -18,7 +18,7 @@ export const BestBuy: Store = {
 		{
 			brand: 'evga',
 			model: 'xc3 ultra',
-			url: 'https://www.bestbuy.com/site/asus-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-strix-graphics-card-black/6432445.p?skuId=6432445',
+			url: 'https://www.bestbuy.com/site/evga-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card/6432400.p?skuId=6432400',
 			oosLabels: ['sold out', 'coming soon']
 		},
 		{
