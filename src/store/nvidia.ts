45279bde30d8da53088def3721c6ce93b9b7d109
George <geman220@gmail.com>
2020-09-18 19:03:27 -0700

    Update nvidia.ts
    Currently if stock is detected via Digital River's API the bot would open the API result page, which doesn't really help purchase a card.  I added the nvidia store page as the cartUrl as a temporary workaround until an automatic add to cart function is implemented

--- src/store/nvidia.ts
+++ src/store/nvidia.ts
@@ -1,7 +1,7 @@
 import {Store} from './store';
 
 export const Nvidia: Store = {
-	cartUrl: '',
+	cartUrl: 'https://www.nvidia.com/en-us/shop/geforce/?page=1&limit=9&locale=en-us&gpu=RTX%203080&gpu_filter=RTX%203090~1,RTX%203080~7,RTX%203070~1,TITAN%20RTX~1,RTX%202080%20Ti~2,RTX%202080%20SUPER~16,RTX%202080~5,RTX%202070%20SUPER~26,RTX%202070~15,RTX%202060%20SUPER~7,RTX%202060~36,GTX%201660%20Ti~29,GTX%201660%20SUPER~4,GTX%201660~11,GTX%201650%20Ti~6,GTX%201650%20SUPER~8,GTX%201650~34',
 	links: [
 		{
 			brand: 'nvidia',
