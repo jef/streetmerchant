5b910650430ad4806b22722efa9a013e72ea47e7
bmalaski <5288648+bmalaski@users.noreply.github.com>
2020-09-20 12:14:56 -0400

    feat(store): add adorama (#104)
    Co-authored-by: Jef LeCompte <jeffreylec@gmail.com>

---
+++ src/store/model/adorama.ts
@@ -0,0 +1,63 @@
+import {Store} from './store';
+
+export const Adorama: Store = {
+	links: [
+		{
+			brand: 'pny',
+			model: 'xlr8',
+			url: 'https://www.adorama.com/pnv301tfxmpb.html',
+			oosLabels: ['temporarily not available', 'out of stock'],
+			captchaLabels: ['please verify you are a human']
+		},
+		{
+			brand: 'msi',
+			model: 'gaming x trio',
+			url: 'https://www.adorama.com/msig380gxt1.html',
+			oosLabels: ['temporarily not available', 'out of stock'],
+			captchaLabels: ['please verify you are a human']
+		},
+		{
+			brand: 'evga',
+			model: 'ftw3 ultra',
+			url: 'https://www.adorama.com/ev10g53897kr.html',
+			oosLabels: ['temporarily not available', 'out of stock'],
+			captchaLabels: ['please verify you are a human']
+		},
+		{
+			brand: 'evga',
+			model: 'xc3 ultra',
+			url: 'https://www.adorama.com/ev10g53885kr.html',
+			oosLabels: ['temporarily not available', 'out of stock'],
+			captchaLabels: ['please verify you are a human']
+		},
+		{
+			brand: 'evga',
+			model: 'ftw3',
+			url: 'https://www.adorama.com/ev10g53895kr.html',
+			oosLabels: ['temporarily not available', 'out of stock'],
+			captchaLabels: ['please verify you are a human']
+		},
+		{
+			brand: 'evga',
+			model: 'xc3',
+			url: 'https://www.adorama.com/ev10g53883kr.html',
+			oosLabels: ['temporarily not available', 'out of stock'],
+			captchaLabels: ['please verify you are a human']
+		},
+		{
+			brand: 'evga',
+			model: 'xc3 black',
+			url: 'https://www.adorama.com/ev10g53881kr.html',
+			oosLabels: ['temporarily not available', 'out of stock'],
+			captchaLabels: ['please verify you are a human']
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 3x oc',
+			url: 'https://www.adorama.com/msig38v3x10c.html',
+			oosLabels: ['temporarily not available', 'out of stock'],
+			captchaLabels: ['please verify you are a human']
+		}
+	],
+	name: 'adorama'
+};
