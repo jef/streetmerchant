0b973e6f331abe73f23877434d7003261b4a62dd
Dan Aita <59552043+aitaDev@users.noreply.github.com>
2020-09-20 03:12:07 -0500

    Update officedepot.ts
    changed spaces with tabs

--- src/store/model/officedepot.ts
+++ src/store/model/officedepot.ts
@@ -1,19 +1,19 @@
 import {Store} from './store';
 
 export const OfficeDepot: Store = {
-    links: [
-        {
-            brand: 'pny',
-            model: 'xlr8',
-            url: 'https://www.officedepot.com/a/products/7189374/PNY-GeForce-RTX-3080-10GB-GDDR6X',
-            oosLabels: ['out of stock']
-        },
-        {
-            brand: 'pny',
-            model: 'xlr8 epic',
-            url: 'https://www.officedepot.com/a/products/7791294/PNY-GeForce-RTX-3080-10GB-GDDR6X/',
-            oosLabels: ['out of stock']
-        }
-    ],
-    name: 'officedepot'
+	links: [
+		{
+			brand: 'pny',
+			model: 'xlr8',
+			url: 'https://www.officedepot.com/a/products/7189374/PNY-GeForce-RTX-3080-10GB-GDDR6X',
+			oosLabels: ['out of stock']
+		},
+		{
+			brand: 'pny',
+			model: 'xlr8 epic',
+			url: 'https://www.officedepot.com/a/products/7791294/PNY-GeForce-RTX-3080-10GB-GDDR6X/',
+			oosLabels: ['out of stock']
+		}
+	],
+	name: 'officedepot'
 };
