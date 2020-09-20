3ef89b32d6c87a8e6ae9b15e47f66a47cfc17ff2
Mark Dietzer <git@doridian.net>
2020-09-20 09:23:04 -0700

    feat(scraping): Change the system for lookups. Added randomized sleep to avoid bot detection better

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,27 @@ import {Store, Stores} from './store/model';
 import {Logger} from './logger';
 import {sendNotification} from './notification';
 import {lookup} from './store';
-import async from 'async';
+import {Browser} from 'puppeteer';
 
 puppeteer.use(stealthPlugin());
 puppeteer.use(adblockerPlugin({blockTrackers: true}));
+
+function getSleepTime() {
+	return Config.browser.minSleep + (Math.random() * (Config.browser.maxSleep - Config.browser.minSleep));
+}
+
+async function tryLookupAndLoop(browser: Browser, store: Store) {
+	Logger.debug(`[${store.name}] Starting lookup...`);
+	try {
+		await lookup(browser, store);
+	} catch (error) {
+		Logger.error(error);
+	}
+
+	const sleepTime = getSleepTime();
+	Logger.debug(`[${store.name}] Lookup done, next one in ${sleepTime} ms`);
+	setTimeout(tryLookupAndLoop, sleepTime, browser, store);
+}
 
 /**
  * Starts the bot.
@@ -23,32 +40,10 @@ async function main() {
 		}
 	});
 
-	const q = async.queue<Store>(async (store: Store, cb) => {
-		setTimeout(async () => {
-			try {
-				Logger.debug(`↗ scraping initialized - ${store.name}`);
-				await lookup(browser, store);
-			} catch (error) {
-				// Ignoring errors; more than likely due to rate limits
-				Logger.error(error);
-			} finally {
-				cb();
-				q.push(store);
-			}
-		}, Config.browser.rateLimitTimeout);
-	}, Stores.length);
-
 	for (const store of Stores) {
 		Logger.debug(store.links);
-		q.push(store);
-		if (Stores.length === 1) {
-			q.push(store);
-		} // Keep from completely draining
+		setTimeout(tryLookupAndLoop, getSleepTime(), browser, store);
 	}
-
-	await q.drain();
-
-	await browser.close();
 }
 
 /**
