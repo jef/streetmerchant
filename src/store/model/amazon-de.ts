f92d4b255723284d5c789b5e56ce5b59ee06072e
manybot <mr.martinho@hotmail.com>
2020-09-21 19:11:25 +0200

    Add files via upload

---
+++ src/store/model/amazon-de.ts
@@ -0,0 +1,95 @@
+import {Store} from './store';
+
+export const AmazonDe: Store = {
+	links: [
+		{
+			brand: 'TEST',
+			model: 'CARD',
+			series: 'debug',
+			url: 'https://www.amazon.com/MSI-GeForce-RTX-2060-Architecture/dp/B07MQ36Z6L/ref=sr_1_4'
+		},
+		{
+			brand: 'pny',
+			model: 'xlr8',
+			series: '3080',
+			url: 'https://www.amazon.de/PNY-GeForce-Gaming-Epic-X-Grafikkarte/dp/B08HBTJMLJ?language=en_GB&ref_=ast_sto_dp'
+		},
+		{
+			brand: 'pny',
+			model: 'xlr8-rgb',
+			series: '3080',
+			url: 'https://www.amazon.de/PNY-GeForce-Gaming-Epic-X-Grafikkarte/dp/B08HBR7QBM?language=en_GB&ref_=ast_sto_dp'
+		},
+		{
+			brand: 'msi',
+			model: 'gaming x trio',
+			series: '3080',
+			url: 'https://www.amazon.de/MSI-3080-10G-Grafikkarte-Express/dp/B08HM4V2DH?language=en_GB&ref_=ast_sto_dp'
+		},
+		{
+			brand: 'evga',
+			model: 'ftw3 ultra',
+			series: '3080',
+			url: 'https://www.amazon.de/EVGA-GeForce-10G-P5-3897-KR-Technology-Backplate/dp/B08HGYXP4C?language=en_GB&ref_=ast_sto_dp'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3 ultra',
+			series: '3080',
+			url: 'https://www.amazon.de/EVGA-GeForce-10G-P5-3885-KR-Cooling-Backplate/dp/B08HJ9XFNM?language=en_GB&ref_=ast_sto_dp'
+		},
+		{
+			brand: 'evga',
+			model: 'ftw3',
+			series: '3080',
+			url: 'https://www.amazon.de/EVGA-GeForce-10G-P5-3895-KR-Technology-Backplate/dp/B08HGBYWQ6?language=en_GB&ref_=ast_sto_dp'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3',
+			series: '3080',
+			url: 'https://www.amazon.de/EVGA-GeForce-10G-P5-3883-KR-Cooling-Backplate/dp/B08HGLN78Q?language=en_GB&ref_=ast_sto_dp'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3 black',
+			series: '3080',
+			url: 'https://www.amazon.de/EVGA-GeForce-Gaming-10G-P5-3881-KR-Cooling/dp/B08HH1BMQQ?language=en_GB&ref_=ast_sto_dp'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'gaming oc',
+			series: '3080',
+			url: 'https://www.amazon.de/Gigabyte-GeForce-Gaming-GDDR6X-Grafikkarte/dp/B08HLZXHZY?language=en_GB&ref_=ast_sto_dp'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle oc',
+			series: '3080',
+			url: 'https://www.amazon.de/Gigabyte-GeForce-Eagle-GDDR6X-Grafikkarte/dp/B08HHZVZ3N?language=en_GB&ref_=ast_sto_dp'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf',
+			series: '3080',
+			url: 'https://www.amazon.de/ASUS-Computer-TUF-RTX3080-O10G-GAMING/dp/B08HN4DSTC?language=en_GB&ref_=ast_sto_dp'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 3x oc',
+			series: '3080',
+			url: 'https://www.amazon.de/MSI-3080-3X-10G-Grafikkarte/dp/B08HM4M621?language=en_GB&ref_=ast_sto_dp'
+		},
+		{
+			brand: 'zotac',
+			model: 'trinity',
+			series: '3080',
+			url: 'https://www.amazon.de/ZOTAC-Gaming-GeForce-3080-Trinity/dp/B08HR1NPPQ?language=en_GB&ref_=ast_sto_dp'
+		}
+	],
+	labels: {
+		captcha: ['enter the characters you see below','geben Sie die unten angezeigten Zeichen ein'],
+		outOfStock: ['Derzeit nicht verfügbar', 'erhältlich bei diesen Verkäufern', 'Momentan nicht verfügbar','currently unavailable', 'available from these sellers']
+	},
+	name: 'amazon-de'
+};
