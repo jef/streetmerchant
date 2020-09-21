faaaed4d474b388cd20ac7aeb904d13bab3ca400
Paolo Moretti <paolo.moretti85@gmail.com>
2020-09-21 21:31:26 +0200

    feat(store): add EVGA EU

--- README.md
+++ README.md
@@ -106,6 +106,7 @@ Here is a list of variables that you can use to customize your newly copied `.en
 | Best Buy | `bestbuy`|
 | B&H | `bandh`|
 | EVGA | `evga`|
+| eu.EVGA.com | `evga-eu`|
 | Micro Center | `microcenter`|
 | Newegg | `newegg`|
 | Newegg.ca | `newegg-ca`|
---
+++ src/store/model/evga-eu.ts
@@ -0,0 +1,35 @@
+import {Store} from './store';
+
+export const EvgaEu: Store = {
+	links: [
+		{
+			brand: 'evga',
+			model: 'xc3 black',
+			series: '3080',
+			url: 'https://eu.evga.com/products/product.aspx?pn=10G-P5-3881-KR'
+		},
+		{
+			brand: 'evga',
+			model: 'ftw3',
+			series: '3080',
+			url: 'https://eu.evga.com/products/product.aspx?pn=10G-P5-3895-KR'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3',
+			series: '3080',
+			url: 'https://eu.evga.com/products/product.aspx?pn=10G-P5-3883-KR'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3 ultra',
+			series: '3080',
+			url: 'https://eu.evga.com/products/product.aspx?pn=10G-P5-3885-KR'
+		}
+	],
+	labels: {
+		outOfStock: ['tbd', 'out of stock']
+	},
+	name: 'evga-eu'
+};
+
