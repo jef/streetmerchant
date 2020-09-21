7176372aafb98ac237a97e9f50053486d8b0ecbf
Scott Cooper <scttcper@gmail.com>
2020-09-21 10:32:10 -0700

    whoops

--- src/store/model/adorama.ts
+++ src/store/model/adorama.ts
@@ -20,24 +20,24 @@ export const Adorama: Store = {
 			series: '3080',
 			url: 'https://www.adorama.com/msig380gxt1.html'
 		},
-		// {
-		// 	brand: 'evga',
-		// 	model: 'ftw3 ultra',
-		// 	series: '3080',
-		// 	url: 'https://www.adorama.com/ev10g53897kr.html'
-		// },
+		{
+			brand: 'evga',
+			model: 'ftw3 ultra',
+			series: '3080',
+			url: 'https://www.adorama.com/ev10g53897kr.html'
+		},
 		{
 			brand: 'evga',
 			model: 'xc3 ultra',
 			series: '3080',
 			url: 'https://www.adorama.com/ev10g53885kr.html'
 		},
-		// {
-		// 	brand: 'evga',
-		// 	model: 'ftw3',
-		// 	series: '3080',
-		// 	url: 'https://www.adorama.com/ev10g53895kr.html'
-		// },
+		{
+			brand: 'evga',
+			model: 'ftw3',
+			series: '3080',
+			url: 'https://www.adorama.com/ev10g53895kr.html'
+		},
 		{
 			brand: 'evga',
 			model: 'xc3',
