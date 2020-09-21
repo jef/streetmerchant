21cc02fd27ce0d06172d73654aa639e6d6d71b44
Colin Farrell <lantis@gmail.com>
2020-09-21 11:45:19 +0200

    Remove trailing comma reported by linter.

--- src/store/model/nvidia-de.ts
+++ src/store/model/nvidia-de.ts
@@ -15,7 +15,7 @@ export const NvidiaDe: Store = {
 			model: 'founders edition',
 			series: '3080',
 			url: 'https://in-and-ru-store-api.uk-e1.cloudhub.io/DR/products/de_de/EUR/5438792300'
-		},
+		}
 	],
 	labels: {
 		outOfStock: ['product_inventory_out_of_stock', 'rate limit exceeded']
