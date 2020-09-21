e8ef127159a7003742b6e50e74e93296d193ea02
Colin Farrell <lantis@gmail.com>
2020-09-21 16:05:24 +0200

    Map new Nvidia file layout to DE file.

--- src/store/model/nvidia-de.ts
+++ src/store/model/nvidia-de.ts
@@ -1,24 +1,122 @@
-import {Store} from './store';
+import { Store } from './store';
+import { Browser, Response } from 'puppeteer';
+import { timestampUrlParameter } from '../timestamp-url-parameter';
+import { Logger } from '../../logger';
+import open from 'open';
+
+const fe2060SuperId = 5394902900;
+const fe3080Id = 5438792300;
+const locale = 'de_de';
+
+const nvidiaApiKey = '9485fa7b159e42edb08a83bde0d83dia';
+
+function digitalRiverStockUrl(id: number): string {
+	return (
+		`https://api.digitalriver.com/v1/shoppers/me/products/${id}/inventory-status?` +
+		`&apiKey=${nvidiaApiKey}` +
+		timestampUrlParameter()
+	);
+}
+
+interface NvidiaSessionTokenJSON {
+	access_token: string;
+}
+
+function nvidiaSessionUrl(): string {
+	return (
+		`https://store.nvidia.com/store/nvidia/SessionToken?format=json&locale=${locale}` +
+		`&apiKey=${nvidiaApiKey}` +
+		timestampUrlParameter()
+	);
+}
+
+function addToCartUrl(id: number, token: string): string {
+	return (
+		'https://api.digitalriver.com/v1/shoppers/me/carts/active/line-items?format=json&method=post' +
+		`&productId=${id}` +
+		`&token=${token}` +
+		'&quantity=1' +
+		timestampUrlParameter()
+	);
+}
+
+function checkoutUrl(token: string): string {
+	return `https://api.digitalriver.com/v1/shoppers/me/carts/active/web-checkout?token=${token}`;
+}
+
+function fallbackCartUrl(): string {
+	return `https://www.nvidia.com/en-us/shop/geforce?${timestampUrlParameter()}`;
+}
+
+function generateCartAction(id: number, cardName: string) {
+	return async (browser: Browser) => {
+		const page = await browser.newPage();
+		Logger.info(
+			`🚀🚀🚀 [nvidia] ${cardName}, starting auto add to cart... 🚀🚀🚀`
+		);
+		let response: Response | null;
+		try {
+			Logger.info(
+				`🚀🚀🚀 [nvidia] ${cardName}, getting access token... 🚀🚀🚀`
+			);
+			response = await page.goto(nvidiaSessionUrl(), {
+				waitUntil: 'networkidle0',
+			});
+			if (response === null) {
+				throw new Error('NvidiaAccessTokenUnavailable');
+			}
+
+			const data = (await response.json()) as NvidiaSessionTokenJSON;
+			const accessToken = data.access_token;
+
+			Logger.info(`🚀🚀🚀 [nvidia] ${cardName}, adding to cart... 🚀🚀🚀`);
+			response = await page.goto(addToCartUrl(id, accessToken), {
+				waitUntil: 'networkidle0',
+			});
+
+			Logger.info(
+				`🚀🚀🚀 [nvidia] ${cardName}, opening checkout page... 🚀🚀🚀`
+			);
+			Logger.info(checkoutUrl(accessToken));
+			await open(checkoutUrl(accessToken));
+		} catch {
+			Logger.error(
+				`✖ [nvidia] ${cardName} could not automatically add to cart, opening page`
+			);
+			await open(fallbackCartUrl());
+		}
+
+		await page.close();
+	};
+}
 
 export const NvidiaDe: Store = {
 	links: [
 		{
-			brand: 'TEST',
-			cartUrl: 'https://www.nvidia.com/de-de/shop/geforce',
-			model: 'CARD',
 			series: 'debug',
-			url: 'https://in-and-ru-store-api.uk-e1.cloudhub.io/DR/products/de_de/EUR/5394902900'
+			brand: 'TEST',
+			model: 'CARD',
+			url: digitalRiverStockUrl(fe2060SuperId),
+			openCartAction: generateCartAction(fe2060SuperId, 'TEST CARD')
 		},
 		{
-			brand: 'nvidia',
-			cartUrl: 'https://www.nvidia.com/de-de/shop/geforce',
-			model: 'founders edition',
 			series: '3080',
-			url: 'https://in-and-ru-store-api.uk-e1.cloudhub.io/DR/products/de_de/EUR/5438792300'
+			brand: 'nvidia',
+			model: 'founders edition',
+			url: digitalRiverStockUrl(fe3080Id),
+			openCartAction: generateCartAction(
+				fe3080Id,
+				'nvidia founders edition 3080'
+			)
 		}
 	],
 	labels: {
-		outOfStock: ['product_inventory_out_of_stock', 'rate limit exceeded']
+		outOfStock: [
+			'product_inventory_out_of_stock',
+			'rate limit exceeded',
+			'request timeout',
+			'resource-not-found'
+		]
 	},
 	name: 'nvidia-de'
 };
