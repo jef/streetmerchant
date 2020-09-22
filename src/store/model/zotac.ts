1de06595bebd4ebcd551c3cd9a9878f8d80e4b39
Andrew Kurniawan <andrew.kurniawan@berkeley.edu>
2020-09-22 13:24:36 -0700

    Added zotac store and 2 zotac cards

---
+++ src/store/model/zotac.ts
@@ -0,0 +1,24 @@
+import {Store} from './store';
+
+export const Zotac: Store = {
+	links: [
+		{
+			brand: 'zotac',
+			model: 'trinity',
+			series: '3080',
+			url: 'https://store.zotac.com/zotac-gaming-geforce-rtx-3080-trinity-zt-a30800d-10p',
+		},
+		{
+			brand: 'zotac',
+			model: 'trinity OC',
+			series: '3080',
+			url: 'https://store.zotac.com/zotac-gaming-geforce-rtx-3080-trinity-oc-zt-a30800j-10p',
+		},
+		
+	],
+	labels: {
+		outOfStock: ['out of stock', 'this process is automatic']
+	},
+	name: 'zotac'
+};
+
