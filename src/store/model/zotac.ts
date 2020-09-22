0e1c5972a691db2b83c6b34146f11ff5c1b1eebb
AndrewKurniawan <andrew.kurniawan@berkeley.edu>
2020-09-22 13:38:09 -0700

    Update zotac.ts
    One extra space apparently?

--- src/store/model/zotac.ts
+++ src/store/model/zotac.ts
@@ -13,7 +13,7 @@ export const Zotac: Store = {
 			model: 'trinity OC',
 			series: '3080',
 			url: 'https://store.zotac.com/zotac-gaming-geforce-rtx-3080-trinity-oc-zt-a30800j-10p'
-		}	
+		}
 	],
 	labels: {
 		outOfStock: ['out of stock', 'this process is automatic']
