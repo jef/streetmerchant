9d3b7d38ab54d812232fdf76a6a6e0da4005bb87
Jef LeCompte <jeffreylec@gmail.com>
2020-09-23 23:49:05 -0400

    chore(store): add more nvidia links
    Signed-off-by: Jef LeCompte <jeffreylec@gmail.com>

--- src/store/model/nvidia.ts
+++ src/store/model/nvidia.ts
@@ -16,19 +16,31 @@ export const Nvidia: Store = {
 			brand: 'test:brand',
 			model: 'test:model',
 			series: 'test:series',
-			url: 'https://www.newegg.com/evga-geforce-rtx-2060-06g-p4-2066-kr/p/N82E16814487488'
+			url: 'https://www.nvidia.com/en-us/shop/geforce/gpu/'
 		},
 		{
 			brand: 'nvidia',
 			model: 'founders edition',
 			series: '3080',
-			url: 'https://www.nvidia.com/en-us/shop/geforce/gpu/?page=1&limit=9&locale=en-us&gpu=RTX%203080&category=GPU&manufacturer=NVIDIA&gpu_filter=RTX%203090~1,RTX%203080~1,RTX%203070~0,TITAN%20RTX~0,RTX%202080%20Ti~0,RTX%202070%20SUPER~0,RTX%202060%20SUPER~0,RTX%202060~0,GTX%201660%20Ti~0,GTX%201660%20SUPER~0,GTX%201660~0,GTX%201650%20SUPER~0,GTX%201650~0'
+			url: 'https://www.nvidia.com/en-us/shop/geforce/gpu/?page=1&limit=9&locale=en-us&category=GPU&gpu=RTX%203080'
+		},
+		{
+			brand: 'nvidia',
+			model: 'founders edition',
+			series: '3080',
+			url: 'https://www.nvidia.com/en-us/geforce/graphics-cards/30-series/rtx-3080'
 		},
 		{
 			brand: 'nvidia',
 			model: 'founders edition',
 			series: '3090',
-			url: 'https://www.nvidia.com/en-us/shop/geforce/gpu/?page=1&limit=9&locale=en-us&gpu=RTX%203090&category=GPU&manufacturer=NVIDIA&gpu_filter=RTX%203090~1,RTX%203080~1,RTX%203070~0,TITAN%20RTX~0,RTX%202080%20Ti~0,RTX%202070%20SUPER~0,RTX%202060%20SUPER~0,RTX%202060~0,GTX%201660%20Ti~0,GTX%201660%20SUPER~0,GTX%201660~0,GTX%201650%20SUPER~0,GTX%201650~0'
+			url: 'https://www.nvidia.com/en-us/shop/geforce/gpu/?page=1&limit=9&locale=en-us&category=GPU&gpu=RTX%203090'
+		},
+		{
+			brand: 'nvidia',
+			model: 'founders edition',
+			series: '3090',
+			url: 'https://www.nvidia.com/en-us/geforce/graphics-cards/30-series/rtx-3090'
 		}
 	],
 	name: 'nvidia'
