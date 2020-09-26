4eba4bbbf9d16fa1ba41a5be811e9a63f8b653de
Andrew Mackrodt <andrewmackrodt@gmail.com>
2020-09-26 07:04:17 +0100

    Update card model detection

--- src/store/model/helpers/card.ts
+++ src/store/model/helpers/card.ts
@@ -72,10 +72,20 @@ export function getProductLinksBuilder(options: LinksBuilderOptions) {
 }
 
 export function parseCard(name: string): Card | null {
-	name = name.replace(/[^\w ]+/g, '').trim();
-	name = name.replace(/\bgraphics card\b/gi, '').trim();
-	name = name.replace(/\b\w+ fan\b/gi, '').trim();
-	name = name.replace(/\s{2,}/g, ' ');
+	name = name.replace(/\w+-\w+-[^ ]+/g, '');
+	name = name.replace(/\([^(]*\)/g, '');
+	name = name.replace(/, .+$/, '');
+	name = name.replace(/ with .+$/, '');
+
+	// Account for incorrect titles, e.g. MSIGeforce
+	name = name.replace(/geforce/i, '');
+
+	name = name.replace(/[^\w ]+/g, '');
+	name = name.replace(/\bgraphics card\b/gi, '');
+	name = name.replace(/\b(?<!founders) edition\b/gi, '');
+	name = name.replace(/\b(series )?bundle\b/gi, '');
+	name = name.replace(/\b\w+ fan\b/gi, '');
+	name = name.replace(/\s{2,}/g, ' ').trim();
 
 	let model = name.split(' ');
 	const brand = model.shift();
@@ -83,6 +93,9 @@ export function parseCard(name: string): Card | null {
 	if (!brand) {
 		return null;
 	}
+
+	// Split non spaced TitleCase words only after extracting brand
+	model = model.join(' ').replace(/([A-Z][a-z]+)([A-Z][a-z]+)/g, '$1 $2').split(' ');
 
 	// Some vendors have oc at the beginning of the product name,
 	// store whether the card contains the term "oc" and remove
@@ -96,8 +109,9 @@ export function parseCard(name: string): Card | null {
 			return false;
 		}
 
-		return !word.match(/^(nvidia|geforce|rtx|amp[ae]re|graphics|card|gpu|pci-?e(xpress)?|ray-?tracing|ray|tracing|core|boost)$/i) &&
-			!word.match(/^(\d+(?:gb?|mhz)?|gb|mhz|g?ddr(\d+x?)?)$/i);
+		return !word.match(/^(nvidia|geforce|ge|force|rtx|amp[ae]re|graphics|card|gpu|pci-?e(xpress)?|ray-?tracing|ray|tracing|core|boost|epicx)$/i) &&
+			!word.match(/^(\d+(?:gb?|mhz)?|gb|mhz|g?ddr(\d+x?)?)$/i) &&
+			!word.match(/^(display ?port|hdmi|vga)$/i);
 	});
 	/* eslint-enable @typescript-eslint/prefer-regexp-exec */
 
