8404164ce19c9fbdfa748be47484475a20c7e846
Xyrec <itsgonnabealongnight@gmail.com>
2020-09-27 15:09:33 +0200

    Fixed amazon.nl test url

--- src/store/model/amazon-nl.ts
+++ src/store/model/amazon-nl.ts
@@ -16,7 +16,7 @@ export const AmazonNl: Store = {
 			brand: 'test:brand',
 			model: 'test:model',
 			series: 'test:series',
-			url: 'https://www.amazon.com/dp/B07MQ36Z6L'
+			url: 'https://www.amazon.nl/dp/B083GGYNQ6/'
 		},
 		{
 			brand: 'msi',
