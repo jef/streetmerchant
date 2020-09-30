b6c56299be086bf55a6d3140aebb4dd9c90d28c2
Grant <gpmillerjax@gmail.com>
2020-09-30 08:44:22 -0400

    feat(store): add Gamestop to stores
    Fixes #365
    
    Tested by running it for hours locally. Seems to be resolving just fine.

---
+++ src/store/model/gamestop.ts
@@ -0,0 +1,27 @@
+import {Store} from './store';
+
+export const Gamestop: Store = {
+	labels: {
+		inStock: {
+			container: '#item_add_cart',
+			text: ['add to cart']
+		}
+	},
+	links: [
+		{
+			brand: 'test:brand',
+			model: 'test:model',
+			series: 'test:series',
+			url: 'https://www.gamestop.com/video-games/pc/components/graphics-cards/products/tuf-gaming-geforce-rtx-3080-graphics-card/11109446.html'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf oc',
+			series: '3080',
+			url: 'https://www.gamestop.com/video-games/pc/components/graphics-cards/products/tuf-gaming-geforce-rtx-3080-graphics-card/11109446.html'
+		}
+	],
+	name: 'gamestop',
+	successStatusCodes: [[0, 399], 404]
+};
+
