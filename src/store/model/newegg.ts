f93bdf339ad82497d8e9315a4fae4b86da7988d5
pntless <pntless@users.noreply.github.com>
2020-10-01 10:49:51 -0400

    chore: adds aorus master 3080 to newegg.ts
    A Gigabyte representative has announced at https://www.reddit.com/r/nvidia/comments/j2uxlc/updated_time_aorus_rtx_3080_master_launch_on/ that the Aorus Master 3080 will go live on Newegg at 6:30pm PST on 2 October. This adds it using the link and product id provided by the representative in that reddit post.

--- src/store/model/newegg.ts
+++ src/store/model/newegg.ts
@@ -96,6 +96,13 @@ export const Newegg: Store = {
 			series: '3080',
 			url: 'https://www.newegg.com/gigabyte-geforce-rtx-3080-gv-n3080eagle-oc-10gd/p/N82E16814932330'
 		},
+				{
+			brand: 'gigabyte',
+			cartUrl: 'https://secure.newegg.com/Shopping/AddtoCart.aspx?Submit=ADD&ItemList=N82E16814137336',
+			model: 'aorus master',
+			series: '3080',
+			url: 'https://www.newegg.com/p/N82E16814137336'
+		},
 		{
 			brand: 'zotac',
 			cartUrl: 'https://secure.newegg.com/Shopping/AddtoCart.aspx?Submit=ADD&ItemList=N82E16814500502',
