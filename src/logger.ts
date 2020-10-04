9d8db464c74a0b9106039932b90ed6cd31824a49
jastheace <myanth@gmail.com>
2020-10-04 18:30:29 -0400

    Update logger.ts

--- src/logger.ts
+++ src/logger.ts
@@ -78,9 +78,9 @@ export const Print = {
 
 		return `ℹ ${buildProductString(link, store)} :: IN STOCK, WAITING`;
 	},
-	maxPrice(link: Link, store: Store, price: number, color?: boolean): string {
+	maxPrice(link: Link, store: Store, price: number, maxPrice: string, color?: boolean): string {
 		if (color) {
-			return '✖ ' + buildProductString(link, store, true) + ' :: ' +	chalk.yellow(`PRICE ${price} EXCEEDS LIMIT ${config.store.maxPrice}`);
+			return '✖ ' + buildProductString(link, store, true) + ' :: ' +	chalk.yellow(`PRICE ${price} EXCEEDS LIMIT ${maxPrice}`);
 		}
 
 		return `✖ ${buildProductString(link, store)} :: PRICE ${price} EXCEEDS LIMIT ${config.store.maxPrice}`;
