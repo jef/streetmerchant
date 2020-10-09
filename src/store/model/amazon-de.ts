3a2a7458ba2f35e5a4845d5f5c2a22545e1600c5
PaddseL <paddsel@gmail.com>
2020-10-09 19:05:06 +0200

    added asus 3090 rog strix/oc to amazon (de)

--- src/store/model/amazon-de.ts
+++ src/store/model/amazon-de.ts
@@ -23,66 +23,6 @@ export const AmazonDe: Store = {
 			url: 'https://www.amazon.de/dp/B083JX52VG/'
 		},
 		{
-			brand: 'pny',
-			model: 'xlr8',
-			series: '3080',
-			url: 'https://www.amazon.de/dp/B08HBTJMLJ'
-		},
-		{
-			brand: 'pny',
-			model: 'xlr8 rgb',
-			series: '3080',
-			url: 'https://www.amazon.de/dp/B08HBR7QBM'
-		},
-		{
-			brand: 'msi',
-			model: 'gaming x trio',
-			series: '3080',
-			url: 'https://www.amazon.de/dp/B08HM4V2DH'
-		},
-		{
-			brand: 'evga',
-			model: 'ftw3 ultra',
-			series: '3080',
-			url: 'https://www.amazon.de/dp/B08HGYXP4C'
-		},
-		{
-			brand: 'evga',
-			model: 'xc3 ultra',
-			series: '3080',
-			url: 'https://www.amazon.de/dp/B08HJ9XFNM'
-		},
-		{
-			brand: 'evga',
-			model: 'ftw3',
-			series: '3080',
-			url: 'https://www.amazon.de/dp/B08HGBYWQ6'
-		},
-		{
-			brand: 'evga',
-			model: 'xc3',
-			series: '3080',
-			url: 'https://www.amazon.de/dp/B08HGLN78Q'
-		},
-		{
-			brand: 'evga',
-			model: 'xc3 black',
-			series: '3080',
-			url: 'https://www.amazon.de/dp/B08HH1BMQQ'
-		},
-		{
-			brand: 'gigabyte',
-			model: 'gaming oc',
-			series: '3080',
-			url: 'https://www.amazon.de/dp/B08HLZXHZY'
-		},
-		{
-			brand: 'gigabyte',
-			model: 'eagle oc',
-			series: '3080',
-			url: 'https://www.amazon.de/dp/B08HHZVZ3N'
-		},
-		{
 			brand: 'asus',
 			model: 'tuf',
 			series: '3080',
@@ -95,16 +35,88 @@ export const AmazonDe: Store = {
 			url: 'https://www.amazon.de/dp/B08HN4DSTC'
 		},
 		{
+			brand: 'evga',
+			model: 'ftw3 ultra',
+			series: '3080',
+			url: 'https://www.amazon.de/dp/B08HGYXP4C'
+		},
+		{
+			brand: 'evga',
+			model: 'ftw3',
+			series: '3080',
+			url: 'https://www.amazon.de/dp/B08HGBYWQ6'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3 ultra',
+			series: '3080',
+			url: 'https://www.amazon.de/dp/B08HJ9XFNM'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3 black',
+			series: '3080',
+			url: 'https://www.amazon.de/dp/B08HH1BMQQ'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3',
+			series: '3080',
+			url: 'https://www.amazon.de/dp/B08HGLN78Q'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle oc',
+			series: '3080',
+			url: 'https://www.amazon.de/dp/B08HHZVZ3N'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'gaming oc',
+			series: '3080',
+			url: 'https://www.amazon.de/dp/B08HLZXHZY'
+		},
+		{
+			brand: 'msi',
+			model: 'gaming x trio',
+			series: '3080',
+			url: 'https://www.amazon.de/dp/B08HM4V2DH'
+		},
+		{
 			brand: 'msi',
 			model: 'ventus 3x oc',
 			series: '3080',
 			url: 'https://www.amazon.de/dp/B08HM4M621'
 		},
 		{
+			brand: 'pny',
+			model: 'xlr8',
+			series: '3080',
+			url: 'https://www.amazon.de/dp/B08HBTJMLJ'
+		},
+		{
+			brand: 'pny',
+			model: 'xlr8 rgb',
+			series: '3080',
+			url: 'https://www.amazon.de/dp/B08HBR7QBM'
+		},
+		{
 			brand: 'zotac',
 			model: 'trinity',
 			series: '3080',
 			url: 'https://www.amazon.de/dp/B08HR1NPPQ'
+		},
+		{
+			brand: 'asus',
+			model: 'rog strix',
+			series: '3090',
+			url: 'https://www.amazon.de/dp/B08HN642LY'
+		},
+		{
+			brand: 'asus',
+			model: 'rog strix oc',
+			series: '3090',
+			url: 'https://www.amazon.de/dp/B08HN51T8Q'
 		}
 	],
 	name: 'amazon-de'
