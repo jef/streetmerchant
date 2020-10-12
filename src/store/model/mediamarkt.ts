0e9acd607ed53439dbdaa47c94237d546318385c
PaddseL <paddsel@gmail.com>
2020-10-12 11:25:51 +0200

    added 3090s to mediamarkt (de)

--- src/store/model/mediamarkt.ts
+++ src/store/model/mediamarkt.ts
@@ -89,6 +89,48 @@ export const Mediamarkt: Store = {
 			model: 'amp extreme holo',
 			series: '3080',
 			url: 'https://www.mediamarkt.de/de/product/-2689452.html'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf',
+			series: '3090',
+			url: 'https://www.mediamarkt.de/de/product/-2681855.html'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf oc',
+			series: '3090',
+			url: 'https://www.mediamarkt.de/de/product/-2681857.html'
+		},
+		{
+			brand: 'asus',
+			model: 'rog strix',
+			series: '3090',
+			url: 'https://www.mediamarkt.de/de/product/-2681863.html'
+		},
+		{
+			brand: 'asus',
+			model: 'rog strix oc',
+			series: '3090',
+			url: 'https://www.mediamarkt.de/de/product/-2681866.html'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'aorus master',
+			series: '3090',
+			url: 'https://www.mediamarkt.de/de/product/-2691441.html'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'aorus xtreme',
+			series: '3090',
+			url: 'https://www.mediamarkt.de/de/product/-2691440.html'
+		},
+		{
+			brand: 'msi',
+			model: 'gaming x trio',
+			series: '3090',
+			url: 'https://www.mediamarkt.de/de/product/-2683226.html'
 		}
 	],
 	name: 'mediamarkt'
