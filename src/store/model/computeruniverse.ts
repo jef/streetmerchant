b917bd8c5c0a0b45f1592bb0e67c7bde426b5813
fubu2k <48277447+fubu2k@users.noreply.github.com>
2020-10-24 15:34:57 +0200

    Update computerverse.ts (#1)
    add gigabyte 3090 aorus master to computeruniverse (de)

--- src/store/model/computeruniverse.ts
+++ src/store/model/computeruniverse.ts
@@ -271,6 +271,12 @@ export const Computeruniverse: Store = {
 			url: 'https://www.computeruniverse.net/de/gigabyte-geforce-rtx3090-gaming-oc-24-gb-oc'
 		},
 		{
+			brand: 'gigabyte',
+			model: 'aorus master',
+			series: '3090',
+			url: 'https://www.computeruniverse.net/de/gigabyte-aorus-geforce-rtx3090-master-24gb'
+		},
+		{
 			brand: 'inno3d',
 			model: 'ichill x3',
 			series: '3090',
