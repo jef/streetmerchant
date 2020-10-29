87f1c0191daa4553318512272e56985c35898f4f
KillahInstinct1 <killahinstinct@steambans.com>
2020-10-29 15:57:30 +0100

    chore: add 3070 cards and correct models on 3080 (#627)
    Co-authored-by: Jef LeCompte <jeffreylec@gmail.com>

--- src/store/model/azerty.ts
+++ src/store/model/azerty.ts
@@ -36,7 +36,7 @@ export const Azerty: Store = {
 		},
 		{
 			brand: 'asus',
-			model: 'tuf gaming',
+			model: 'tuf',
 			series: '3080',
 			url: 'https://azerty.nl/product/asus/4346679/tuf-gaming-geforce-rtx-3080-grafische-kaart-gf-rtx-3080'
 		},
@@ -48,13 +48,13 @@ export const Azerty: Store = {
 		},
 		{
 			brand: 'asus',
-			model: 'tuf oc gaming',
+			model: 'tuf oc',
 			series: '3080',
 			url: 'https://azerty.nl/product/asus/4348176/tuf-gaming-geforce-rtx-3080-oc-grafische-kaart-gf-rtx-3080'
 		},
 		{
 			brand: 'asus',
-			model: 'rog strix oc gaming',
+			model: 'rog strix oc',
 			series: '3080',
 			url: 'https://azerty.nl/product/asus/4348174/rog-strix-geforce-rtx-3080-oc-grafische-kaart-gf-rtx-3080'
 		},
@@ -75,6 +75,90 @@ export const Azerty: Store = {
 			model: 'xlr8 rgb',
 			series: '3080',
 			url: 'https://azerty.nl/product/pny/4342269/geforce-rtx-3080-xlr8-gaming-revel-epic-x-rgb-triple-fan-gaming-edition-grafische-kaart-gf-rtx-3080'
+		},
+		{
+			brand: 'asus',
+			model: 'rog strix oc',
+			series: '3070',
+			url: 'https://azerty.nl/product/asus/4363892/rog-strix-gaming-geforce-rtx-3070-o8g-videokaart-8-gb-gddr6'
+		},
+		{
+			brand: 'asus',
+			model: 'rog strix',
+			series: '3070',
+			url: 'https://azerty.nl/product/asus/4373096/rog-strix-gaming-geforce-rtx-3070-8g-videokaart-8-gb-gddr6'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf oc',
+			series: '3070',
+			url: 'https://azerty.nl/product/asus/4363910/tuf-gaming-geforce-rtx-3070-oc-videokaart-8-gb-gddr6'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf',
+			series: '3070',
+			url: 'https://azerty.nl/product/asus/4373099/tuf-gaming-geforce-rtx-3070-videokaart-8-gb-gddr6'
+		},
+		{
+			brand: 'inno3d',
+			model: 'ichill x4',
+			series: '3070',
+			url: 'https://azerty.nl/product/inno3d/4369725/geforce-rtx-3070-ichill-x4-videokaart-8-gb-ddr6'
+		},
+		{
+			brand: 'inno3d',
+			model: 'ichill x3',
+			series: '3070',
+			url: 'https://azerty.nl/product/inno3d/4369726/geforce-rtx-3070-ichill-x3-videokaart-8-gb-ddr6'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 3x oc',
+			series: '3070',
+			url: 'https://azerty.nl/product/msi/4374747/geforce-rtx-3070-ventus-3x-oc-videokaart-8-gb-gddr6'
+		},
+		{
+			brand: 'msi',
+			model: 'gaming x trio',
+			series: '3070',
+			url: 'https://azerty.nl/product/msi/4365398/geforce-rtx-3070-gaming-x-trio-videokaart-8-gb-gddr6'
+		},
+		{
+			brand: 'pny',
+			model: 'dual fan',
+			series: '3070',
+			url: 'https://azerty.nl/product/pny/4342270/uprising-dual-fan-geforce-rtx-3070'
+		},
+		{
+			brand: 'pny',
+			model: 'xlr8 rgb',
+			series: '3070',
+			url: 'https://azerty.nl/product/pny/4342271/epic-x-rgb-geforce-rtx-3070-triple-fan-gaming-edition-xlr8--videokaart-8-gb-gddr6x'
+		},
+		{
+			brand: 'evga',
+			model: 'ftw3',
+			series: '3070',
+			url: 'https://azerty.nl/product/evga/4377247/geforce-rtx-3070-ftw3-videokaart-8-gb-gddr6'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3',
+			series: '3070',
+			url: 'https://azerty.nl/product/evga/4377248/xc3-geforce-rtx-3070-xc3-videokaart-8-gb-gddr6'
+		},
+		{
+			brand: 'evga',
+			model: 'ftw3 ultra',
+			series: '3070',
+			url: 'https://azerty.nl/product/evga/4377250/ftw3-ultra-geforce-rtx-3070-videokaart-8-gb-gddr6'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3 black',
+			series: '3070',
+			url: 'https://azerty.nl/product/evga/4377252/xc3-black-geforce-rtx-3070-videokaart-8-gb-gddr6'
 		}
 	],
 	name: 'azerty'
