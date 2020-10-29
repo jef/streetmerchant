1bcf8cb7ac2588be85e1113dd490c9618e490049
PaddseL <paddsel@gmail.com>
2020-10-29 15:26:50 +0100

    added several 3070 cards to computeruniverse (de)

--- src/store/model/computeruniverse.ts
+++ src/store/model/computeruniverse.ts
@@ -20,9 +20,33 @@ export const Computeruniverse: Store = {
 		},
 		{
 			brand: 'asus',
+			model: 'dual',
+			series: '3070',
+			url: 'https://www.computeruniverse.net/de/asus-geforce-rtx3070-dual-rtx3070-8g-8-gb-enthusiast-grafikkarte'
+		},
+		{
+			brand: 'asus',
+			model: 'dual oc',
+			series: '3070',
+			url: 'https://www.computeruniverse.net/de/asus-geforce-rtx3070-dual-oc-dual-rtx3070-o8g-8-gb-oc-enthusiast-grafikkarte'
+		},
+		{
+			brand: 'asus',
+			model: 'rog strix',
+			series: '3070',
+			url: 'https://www.computeruniverse.net/de/asus-rog-strix-geforce-rtx3070-gaming-8-gb-enthusiast-grafikkarte'
+		},
+		{
+			brand: 'asus',
 			model: 'rog strix oc',
 			series: '3070',
 			url: 'https://www.computeruniverse.net/de/asus-rog-strix-geforce-rtx3070-gaming-oc-8-gb-oc-enthusiast-grafikkarte'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3 black',
+			series: '3070',
+			url: 'https://www.computeruniverse.net/de/evga-geforce-rtx3070-xc3-black'
 		},
 		{
 			brand: 'gainward',
@@ -61,10 +85,40 @@ export const Computeruniverse: Store = {
 			url: 'https://www.computeruniverse.net/de/inno3d-geforce-rtx3070-ichill-x4'
 		},
 		{
+			brand: 'msi',
+			model: 'gaming x trio',
+			series: '3070',
+			url: 'https://www.computeruniverse.net/de/msi-geforce-rtx3070-gaming-x-trio-8-gb-enthusiast-grafikkarte'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 2x oc',
+			series: '3070',
+			url: 'https://www.computeruniverse.net/de/msi-geforce-rtx3070-ventus-2x-oc-8-gb-oc-enthusiast-grafikkarte'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 3x oc',
+			series: '3070',
+			url: 'https://www.computeruniverse.net/de/msi-geforce-rtx3070-ventus-3x-oc-8-gb-oc-enthusiast-grafikkarte'
+		},
+		{
 			brand: 'palit',
 			model: 'gaming pro',
 			series: '3070',
 			url: 'https://www.computeruniverse.net/de/palit-geforce-rtx3070-gaming-pro-8-gb'
+		},
+		{
+			brand: 'pny',
+			model: 'dual fan',
+			series: '3070',
+			url: 'https://www.computeruniverse.net/de/pny-geforce-rtx3070-m-dual-8-gb'
+		},
+		{
+			brand: 'pny',
+			model: 'xlr8 rgb',
+			series: '3070',
+			url: 'https://www.computeruniverse.net/de/pny-geforce-rtx3070-xlr8-gaming-epic-x-rgb-p-8-gb-enthusiast-grafikkarte'
 		},
 		{
 			brand: 'zotac',
