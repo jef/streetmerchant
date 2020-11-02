a3650ec5578406c1813b0ea061f4437960c4922e
Tomson124 <tomson124@outlook.de>
2020-11-02 22:17:00 +0100

    Add more 3070 cards to notebooksbilliger

--- src/store/model/notebooksbilliger.ts
+++ src/store/model/notebooksbilliger.ts
@@ -19,6 +19,10 @@ export const Notebooksbilliger: Store = {
 			text: ['dieses produkt ist leider ausverkauft.']
 		},
 		{
+			container: '.just_inStore',
+			text: ['dieses Produkt kann nur in unseren unten genannten Stores gekauft werden.']
+		},
+		{
 			container: '#product_error_text',
 			text: ['leider ist dieser artikel nicht mehr verfügbar.']
 		}]
@@ -37,10 +41,64 @@ export const Notebooksbilliger: Store = {
 			url: 'https://www.notebooksbilliger.de/inno3d+geforce+rtx+3070+ichill+x3+grafikkarte+684162'
 		},
 		{
+			brand: 'inno3d',
+			model: 'twin x2',
+			series: '3070',
+			url: 'https://www.notebooksbilliger.de/inno3d+geforce+rtx+3070+twin+x2+grafikkarte+685496'
+		},
+		{
+			brand: 'inno3d',
+			model: 'twin x2 oc',
+			series: '3070',
+			url: 'https://www.notebooksbilliger.de/inno3d+geforce+rtx+3070+twin+x2+oc+grafikkarte+685500'
+		},
+		{
 			brand: 'nvidia',
 			model: 'founders edition',
 			series: '3070',
 			url: 'https://www.notebooksbilliger.de/nvidia+geforce+rtx+3070+founders+edition+685357'
+		},
+		{
+			brand: 'gainward',
+			model: 'phoenix',
+			series: '3070',
+			url: 'https://www.notebooksbilliger.de/gainward+geforce+rtx+3070+phoenix+8gb+gddr6+grafikkarte+685073'
+		},
+		{
+			brand: 'gainward',
+			model: 'phoenix gs',
+			series: '3070',
+			url: 'https://www.notebooksbilliger.de/gainward+geforce+rtx+3070+phoenix+gs+8gb+gddr6+grafikkarte+681575'
+		},
+		{
+			brand: 'pny',
+			model: 'uprising',
+			series: '3070',
+			url: 'https://www.notebooksbilliger.de/pny+geforce+rtx+3070+8gb+uprising+dual+fan+edition+grafikkarte+685520'
+		},
+		{
+			brand: 'pny',
+			model: 'xlr8 rgb',
+			series: '3070',
+			url: 'https://www.notebooksbilliger.de/pny+geforce+rtx+3070+8gb+xlr8+gaming+revel+epic+x+rgb+685560'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf',
+			series: '3070',
+			url: 'https://www.notebooksbilliger.de/asus+tuf+gaming+geforce+rtx+3070+8gb+grafikkarte+685524'
+		},
+		{
+			brand: 'zotac',
+			model: 'twin edge',
+			series: '3070',
+			url: 'https://www.notebooksbilliger.de/zotac+gaming+geforce+rtx+3070+twin+edge+8gb+gddr6+grafikkarte+677561'
+		},
+		{
+			brand: 'zotac',
+			model: 'twin edge oc',
+			series: '3070',
+			url: 'https://www.notebooksbilliger.de/zotac+gaming+geforce+rtx+3070+twin+edge+oc+8gb+gddr6+grafikkarte+684216'
 		},
 		{
 			brand: 'gainward',
