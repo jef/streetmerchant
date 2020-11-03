e62eb72056b6e79379a0d62ba2d1f1c6b7e031a7
Thomas Wolfgruber <Tomson124@users.noreply.github.com>
2020-11-03 05:44:41 +0100

    chore: add more 3070 cards to media markt and notebooksbilliger, fix false positives on media markt (#665)

--- src/store/model/mediamarkt.ts
+++ src/store/model/mediamarkt.ts
@@ -6,10 +6,14 @@ export const Mediamarkt: Store = {
 			container: 'span[font-family="price"]',
 			euroFormat: false
 		},
-		outOfStock: {
+		outOfStock: [{
 			container: '#root',
 			text: ['dieser artikel ist aktuell nicht verfügbar.']
-		}
+		},
+		{
+			container: '#root',
+			text: ['leider keine Lieferung möglich']
+		}]
 	},
 	links: [
 		{
@@ -20,9 +24,33 @@ export const Mediamarkt: Store = {
 		},
 		{
 			brand: 'asus',
+			model: 'rog strix',
+			series: '3070',
+			url: 'https://www.mediamarkt.de/de/product/-2691243.html'
+		},
+		{
+			brand: 'asus',
 			model: 'rog strix oc',
 			series: '3070',
 			url: 'https://www.mediamarkt.de/de/product/-2691244.html'
+		},
+		{
+			brand: 'asus',
+			model: 'dual',
+			series: '3070',
+			url: 'https://www.mediamarkt.de/de/product/-2691245.html'
+		},
+		{
+			brand: 'asus',
+			model: 'dual oc',
+			series: '3070',
+			url: 'https://www.mediamarkt.de/de/product/-2691246.html'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf oc',
+			series: '3070',
+			url: 'https://www.mediamarkt.de/de/product/-2691247.html'
 		},
 		{
 			brand: 'gigabyte',
@@ -31,16 +59,64 @@ export const Mediamarkt: Store = {
 			url: 'https://www.mediamarkt.de/de/product/-2691439.html'
 		},
 		{
+			brand: 'gigabyte',
+			model: 'eagle oc',
+			series: '3070',
+			url: 'https://www.mediamarkt.de/de/product/-2691444.html'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'aorus master',
+			series: '3070',
+			url: 'https://www.mediamarkt.de/de/product/-2696163.html'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'vision oc',
+			series: '3070',
+			url: 'https://www.mediamarkt.de/de/product/-2696164.html'
+		},
+		{
+			brand: 'msi',
+			model: 'gaming x trio',
+			series: '3070',
+			url: 'https://www.mediamarkt.de/de/product/-2694894.html'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 3x oc',
+			series: '3070',
+			url: 'https://www.mediamarkt.de/de/product/-2694896.html'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 2x oc',
+			series: '3070',
+			url: 'https://www.mediamarkt.de/de/product/-2694898.html'
+		},
+		{
 			brand: 'inno3d',
 			model: 'ichill x3',
 			series: '3070',
 			url: 'https://www.mediamarkt.de/de/product/-2695942.html'
 		},
 		{
+			brand: 'inno3d',
+			model: 'ichill x4',
+			series: '3070',
+			url: 'https://www.mediamarkt.de/de/product/-2695941.html'
+		},
+		{
 			brand: 'zotac',
 			model: 'twin edge',
 			series: '3070',
 			url: 'https://www.mediamarkt.de/de/product/-2691365.html'
+		},
+		{
+			brand: 'zotac',
+			model: 'twin edge oc',
+			series: '3070',
+			url: 'https://www.mediamarkt.de/de/product/-2695671.html'
 		},
 		{
 			brand: 'asus',
