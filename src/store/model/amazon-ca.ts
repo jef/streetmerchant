760ddf5d8523e28e294146643dda16e105176dff
serg06 <serkhas@hotmail.com>
2020-11-05 23:27:01 -0500

    feat(store): add zen 3 to more stores (#698)

--- src/store/model/amazon-ca.ts
+++ src/store/model/amazon-ca.ts
@@ -129,6 +129,30 @@ export const AmazonCa: Store = {
 			model: 'tuf oc',
 			series: '3090',
 			url: 'https://www.amazon.ca/dp/B08HJLLF7G'
+		},
+		{
+			brand: 'amd',
+			model: '5600x',
+			series: 'ryzen5600',
+			url: 'https://www.amazon.ca/dp/B08166SLDF'
+		},
+		{
+			brand: 'amd',
+			model: '5800x',
+			series: 'ryzen5800',
+			url: 'https://www.amazon.ca/dp/B0815XFSGK'
+		},
+		{
+			brand: 'amd',
+			model: '5900x',
+			series: 'ryzen5900',
+			url: 'https://www.amazon.ca/dp/B08164VTWH'
+		},
+		{
+			brand: 'amd',
+			model: '5950x',
+			series: 'ryzen5950',
+			url: 'https://www.amazon.ca/dp/B0815Y8J9N'
 		}
 	],
 	name: 'amazon-ca'
