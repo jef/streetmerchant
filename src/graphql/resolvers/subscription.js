8f44336e9c9ae4801c1375a36933f173093f9b11
Timothy Park <thetimothyp@gmail.com>
2020-11-06 21:14:22 -0800

    feat(api): allow client to filter by store, brand, model, series

--- src/graphql/resolvers/subscription.js
+++ src/graphql/resolvers/subscription.js
@@ -1,3 +1,5 @@
+import {withFilter} from 'apollo-server';
+
 const feedEntrySubscribe = (_, __, context) => {
 	return context.pubsub.asyncIterator('FEED_ENTRY');
 };
@@ -6,7 +8,28 @@ const feedEntry = {
 	resolve: payload => {
 		return payload;
 	},
-	subscribe: feedEntrySubscribe
+	subscribe: withFilter(
+		feedEntrySubscribe,
+		(payload, variables) => {
+			if (variables.brand && variables.brand !== payload.link.brand) {
+				return false;
+			}
+
+			if (variables.series && variables.series !== payload.link.series) {
+				return false;
+			}
+
+			if (variables.model && variables.model !== payload.link.model) {
+				return false;
+			}
+
+			if (variables.storeName && variables.storeName !== payload.store.name) {
+				return false;
+			}
+
+			return true;
+		}
+	)
 };
 
 const Subscription = {feedEntry};
