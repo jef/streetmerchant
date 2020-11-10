32c1d88983254bf0c46236ad59c61ca4265edaca
James Hinton <65028523+J-Hinton@users.noreply.github.com>
2020-11-10 00:48:37 +0000

    chore: add test for game store

--- src/store/model/game.ts
+++ src/store/model/game.ts
@@ -17,6 +17,12 @@ export const Game: Store = {
 	},
 	links: [
 		{
+			brand: 'test:brand',
+			model: 'test:model',
+			series: 'test:series',
+			url: 'https://www.game.co.uk/en/ea-sports-fifa-21-500gb-ps4-bundle-2832947'
+		},
+		{
 			brand: 'sony',
 			model: 'ps5 console',
 			series: 'sonyps5c',
