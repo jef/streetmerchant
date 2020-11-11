b18732192a04388ffa90ba53bd8eba91b8d191c1
Garret Picchioni <garret@picchioni.org>
2020-11-10 23:09:51 -0800

    chore(amazon): fix url and include cart url for missing products

--- src/store/model/amazon.ts
+++ src/store/model/amazon.ts
@@ -251,28 +251,28 @@ export const Amazon: Store = {
 			cartUrl: 'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B08166SLDF&Quantity.1=1',
 			model: '5600x',
 			series: 'ryzen5600',
-			url: 'https://www.amazon.com/dp/product/B08166SLDF'
+			url: 'https://www.amazon.com/dp/B08166SLDF'
 		},
 		{
 			brand: 'amd',
 			cartUrl: 'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0815XFSGK&Quantity.1=1',
 			model: '5800x',
 			series: 'ryzen5800',
-			url: 'https://www.amazon.com/dp/product/B0815XFSGK'
+			url: 'https://www.amazon.com/dp/B0815XFSGK'
 		},
 		{
 			brand: 'amd',
 			cartUrl: 'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B08164VTWH&Quantity.1=1',
 			model: '5900x',
 			series: 'ryzen5900',
-			url: 'https://www.amazon.com/dp/product/B08164VTWH'
+			url: 'https://www.amazon.com/dp/B08164VTWH'
 		},
 		{
 			brand: 'amd',
 			cartUrl: 'https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=B0815Y8J9N&Quantity.1=1',
 			model: '5950x',
 			series: 'ryzen5950',
-			url: 'https://www.amazon.com/dp/product/B0815Y8J9N'
+			url: 'https://www.amazon.com/dp/B0815Y8J9N'
 		},
 		{
 			brand: 'sony',
