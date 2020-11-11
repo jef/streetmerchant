c2b1e1bbc221de53d5f5e5b16845b15b10ee958c
Cristian <kiyoma19@gmail.com>
2020-11-11 17:53:45 +0100

    Add 3090 series to vsgamers store

--- src/store/model/vsgamers.ts
+++ src/store/model/vsgamers.ts
@@ -168,6 +168,60 @@ export const vsgamers: Store = {
 			series: '3080',
 			url: 'https://www.vsgamers.es/product/tarjeta-grafica-zotac-geforce-rtx-3080-trinity-10-gb-gddr6x'
         },
+        {
+			brand: 'zotac',
+			model: 'trinity',
+			series: '3090',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-zotac-geforce-rtx-3090-trinity-24-gb-gddr6x'
+        },
+        {
+			brand: 'asus',
+			model: 'tuf',
+			series: '3090',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-asus-tuf-gaming-geforce-rtx-3090-24g-24-gb-gddr6x'
+        },
+        {
+			brand: 'asus',
+			model: 'tuf oc',
+			series: '3090',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-asus-tuf-gaming-geforce-rtx-3090-oc-24g-24-gb-gddr6x'
+        },
+        {
+			brand: 'asus',
+			model: 'strix',
+			series: '3090',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-asus-rog-strix-geforce-rtx-3090-gaming-24g-24-gb-gddr6x'
+        },
+        {
+			brand: 'msi',
+			model: 'ventus 3x oc',
+			series: '3090',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-msi-geforce-rtx-3090-ventus-3x-24g-oc-24-gb-gddr6x'
+        },
+        {
+			brand: 'msi',
+			model: 'gaming x trio',
+			series: '3090',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-msi-geforce-rtx-3090-gaming-x-trio-24g-24-gb-gddr6x'
+        },
+        {
+			brand: 'asus',
+			model: 'strix oc',
+			series: '3090',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-asus-rog-strix--geforce-rtxr-3090-gaming-oc-24gb-gddr6x'
+        },
+        {
+			brand: 'gigabyte',
+			model: 'eagle oc',
+			series: '3090',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-gigabyte-geforce-rtx-3090-eagle-oc-24g-24-gb-gddr6x'
+        },
+        {
+			brand: 'gigabyte',
+			model: 'gaming oc',
+			series: '3090',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-gigabyte-geforce-rtx-3090-gaming-oc-24g-24-gb-gddr6x'
+        }
         
     ],
 	name: 'vsgamers'
