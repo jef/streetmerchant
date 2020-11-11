d202cc0c1a2b16333a720628a685ba3cc875f33b
Cristian <kiyoma19@gmail.com>
2020-11-11 17:06:10 +0100

    Add 3070 series

--- src/store/model/vsgamers.ts
+++ src/store/model/vsgamers.ts
@@ -4,7 +4,7 @@ export const vsgamers: Store = {
 	labels: {
 		inStock: {
 			container: '#vs-product-sheet-dashboard',
-			text: ['COMPRAR']
+			text: ['COMPRAR', 'RESERVAR']
 		},
 		outOfStock: {
 			container: '#vs-product-sheet-dashboard',
@@ -26,10 +26,88 @@ export const vsgamers: Store = {
         },
         {
 			brand: 'amd',
-			model: '5600x',
-			series: 'ryzen5600',
-			url: 'https://www.vsgamers.es/product/procesador-amd-ryzen-5-5600x-37-ghz'
-        }
+			model: '5800x',
+			series: 'ryzen5800',
+			url: 'https://www.vsgamers.es/product/procesador-amd-ryzen-7-5800x-38-ghz'
+        },
+        {
+			brand: 'amd',
+			model: '5900x',
+			series: 'ryzen5900',
+			url: 'https://www.vsgamers.es/product/procesador-amd-ryzen-9-5900x-37-ghz'
+        },
+        {
+			brand: 'amd',
+			model: '5950x',
+			series: 'ryzen5950',
+			url: 'https://www.vsgamers.es/product/procesador-amd-ryzen-9-5950x-34-ghz'
+        },
+        {
+			brand: 'zotac',
+			model: 'twin edge',
+			series: '3070',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-zotac-gaming-geforce-rtx-3070-twin-edge-oc-8-gb-gddr6'
+        },
+        {
+			brand: 'asus',
+			model: 'tuf',
+			series: '3070',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-asus-tuf-gaming-geforce-rtx-3070-oc-8gb-gddr6'
+        },
+        {
+			brand: 'gigabyte',
+			model: 'gaming oc',
+			series: '3070',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-gigabyte-geforce-rtx-3070-gaming-oc-8-gb-gddr6'
+        },
+        {
+			brand: 'gigabyte',
+			model: 'gaming oc',
+			series: '3070',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-gigabyte-geforce-rtx-3070-gaming-oc-8-gb-gddr6'
+        },
+        {
+			brand: 'msi',
+			model: 'gaming x trio',
+			series: '3070',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-msi-geforce-rtxr-3070-gaming-x-trio'
+        },
+        {
+			brand: 'msi',
+			model: 'ventus 2x oc',
+			series: '3070',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-msi-geforce-rtxr-3070-ventus-2x-oc-8gb-gddr6'
+        },
+        {
+			brand: 'msi',
+			model: 'ventus 3x oc',
+			series: '3070',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-msi-geforce-rtxr-3070-ventus-3x-oc-8gb-gddr6'
+        },
+        {
+			brand: 'asus',
+			model: 'dual',
+			series: '3070',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-asus-dual-geforce-rtx-3070-gaming-8g-8-gb-gddr6'
+        },
+        {
+			brand: 'asus',
+			model: 'dual oc',
+			series: '3070',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-asus-dual-geforce-rtx-3070-gaming-oc-8g-8-gb-gddr6'
+        },
+        {
+			brand: 'asus',
+			model: 'strix oc',
+			series: '3070',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-asus-rog-strix-geforce-rtx-3070-gaming-oc-8g-8-gb-gddr6x'
+        },
+        {
+			brand: 'asus',
+			model: 'strix',
+			series: '3070',
+			url: 'https://www.vsgamers.es/product/tarjeta-grafica-asus-rog-strix-geforce-rtx-3070-gaming-8g-8-gb-gddr6x'
+        },
     ],
 	name: 'vsgamers'
 };
