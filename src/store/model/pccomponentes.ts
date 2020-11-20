3dbe4c9fb575078d49405009130183ce6c24f81e
Cristian <kiyoma19@gmail.com>
2020-11-20 01:11:06 +0100

    update linting

--- src/store/model/pccomponentes.ts
+++ src/store/model/pccomponentes.ts
@@ -178,7 +178,9 @@ export const PCComponentes: Store = {
 			brand: 'amd',
 			model: '5900x',
 			series: 'ryzen5900',
-			url: 'https://www.pccomponentes.com/amd-ryzen-9-5900x-37-ghz'
+			url: 
+			
+			'https://www.pccomponentes.com/amd-ryzen-9-5900x-37-ghz'
 
 		},
 		{
