3286d624a134d2f460b5d79cb19c6b0801a330db
Enrico Martelli <mrcnrt95e30@gmail.com>
2020-11-27 20:09:31 +0100

    chore(store): add amd-it store and RX 6800 XT (#974)

--- src/store/model/index.ts
+++ src/store/model/index.ts
@@ -12,6 +12,7 @@ import {AmazonNl} from './amazon-nl';
 import {AmazonUk} from './amazon-uk';
 import {Amd} from './amd';
 import {AmdDe} from './amd-de';
+import {AmdIt} from './amd-it';
 import {Aria} from './aria';
 import {Arlt} from './arlt';
 import {Asus} from './asus';
@@ -83,6 +84,7 @@ export const storeList = new Map([
 	[AmazonIt.name, AmazonIt],
 	[Amd.name, Amd],
 	[AmdDe.name, AmdDe],
+	[AmdIt.name, AmdIt],
 	[Aria.name, Aria],
 	[Arlt.name, Arlt],
 	[Asus.name, Asus],
