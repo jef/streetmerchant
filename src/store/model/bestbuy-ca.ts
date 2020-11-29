5fe0f58978dd48b071c5f9a3de5ad1157e979762
JP Confessor <evilsk4ter@gmail.com>
2020-11-29 14:40:22 -0500

    fixing identation: From spaces to tabs.

--- src/store/model/bestbuy-ca.ts
+++ src/store/model/bestbuy-ca.ts
@@ -111,18 +111,18 @@ export const BestBuyCa: Store = {
 			url:
 				'https://www.bestbuy.ca/en-ca/product/nvidia-geforce-rtx-3070-8gb-gddr6-video-card-only-at-best-buy/15078017?intl=nosplash'
 		},
-        {
-            brand: 'sony',
-            model: 'ps5 console',
-            series: 'sonyps5c',
-            url: 'https://www.bestbuy.ca/en-ca/product/playstation-5-console-online-only/14962185?intl=nosplash'
-        },
-        {
-            brand: 'sony',
-            model: 'ps5 console',
-            series: 'sonyps5de',
-            url: 'https://www.bestbuy.ca/en-ca/product/playstation-5-digital-edition-console-online-only/14962184?intl=nosplash'
-        }
+		{
+			brand: 'sony',
+			model: 'ps5 console',
+			series: 'sonyps5c',
+			url: 'https://www.bestbuy.ca/en-ca/product/playstation-5-console-online-only/14962185?intl=nosplash'
+		},
+		{
+			brand: 'sony',
+			model: 'ps5 console',
+			series: 'sonyps5de',
+			url: 'https://www.bestbuy.ca/en-ca/product/playstation-5-digital-edition-console-online-only/14962184?intl=nosplash'
+		}
 	],
 	name: 'bestbuy-ca',
 	waitUntil: 'domcontentloaded'
