711b97ca9c361ed047f2468b19d92eb96cacdf5f
Jef LeCompte <jeffreylec@gmail.com>
2020-11-30 09:21:23 -0500

    fix: linting

--- src/store/model/caseking.ts
+++ src/store/model/caseking.ts
@@ -343,42 +343,42 @@ export const Caseking: Store = {
 			brand: 'sapphire',
 			model: 'nitro oc se',
 			series: 'rx6800xt',
-			url: 
+			url:
 				'https://www.caseking.de/sapphire-nitro-radeon-rx-6800-xt-oc-16g-16384-mb-gddr6-gcsp-215.html'
 		},
 		{
 			brand: 'sapphire',
 			model: 'nitro oc',
 			series: 'rx6800xt',
-			url: 
+			url:
 				'https://www.caseking.de/sapphire-nitro-radeon-rx-6800-xt-oc-se-16g-16384-mb-gddr6-gcsp-214.html'
 		},
 		{
 			brand: 'gigabyte',
 			model: 'gaming oc',
 			series: 'rx6800xt',
-			url: 
+			url:
 				'https://www.caseking.de/gigabyte-radeon-rx-6800-xt-gaming-oc-16g-16384-mb-gddr6-gcgb-356.html'
 		},
 		{
 			brand: 'gigabyte',
 			model: 'gaming',
 			series: 'rx6800xt',
-			url: 
+			url:
 				'https://www.caseking.de/gigabyte-radeon-rx-6800-xt-16g-16384-mb-gddr6-gcgb-357.html'
 		},
 		{
 			brand: 'asus',
 			model: 'strix lc',
 			series: 'rx6800xt',
-			url: 
+			url:
 				'https://www.caseking.de/asus-radeon-rx-6800-xt-rog-strix-lc-o16g-16384-mb-gddr6-gcas-416.html'
 		},
 		{
 			brand: 'asus',
 			model: 'tuf',
 			series: 'rx6800xt',
-			url: 
+			url:
 				'https://www.caseking.de/asus-radeon-rx-6800-xt-tuf-o16g-16384-mb-gddr6-gcas-417.html'
 		}
 	],
