9ffd46f6e30335232d717e3066b6b626d4d62f9b
fnk93 <f.krueger1993@gmail.com>
2020-12-01 10:45:03 +0100

    add store Medimax

--- src/store/model/index.ts
+++ src/store/model/index.ts
@@ -46,6 +46,7 @@ import {Gamestop} from './gamestop';
 import {GamestopDE} from './gamestop-de';
 import {Kabum} from './kabum';
 import {Mediamarkt} from './mediamarkt';
+import {Medimax} from './medimax';
 import {MemoryExpress} from './memoryexpress';
 import {MicroCenter} from './microcenter';
 import {Mindfactory} from './mindfactory';
@@ -124,6 +125,7 @@ export const storeList = new Map([
 	[GamestopDE.name, GamestopDE],
 	[Kabum.name, Kabum],
 	[Mediamarkt.name, Mediamarkt],
+	[Medimax.name, Medimax],
 	[MemoryExpress.name, MemoryExpress],
 	[MicroCenter.name, MicroCenter],
 	[Mindfactory.name, Mindfactory],
