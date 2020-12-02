2c1bf008d45a10154fa874a53aa66902f22eae7d
Jef LeCompte <jeffreylec@gmail.com>
2020-12-02 13:21:52 -0500

    chore: linting

--- src/store/model/bandh.ts
+++ src/store/model/bandh.ts
@@ -73,31 +73,36 @@ export const BAndH: Store = {
 			brand: 'gigabyte',
 			model: 'eagle oc',
 			series: '3060ti',
-			url: 'https://www.bhphotovideo.com/c/product/1606948-REG/gigabyte_gv_n306teagle_oc_8gd_rtx_3060_ti_eagle.html'
+			url:
+				'https://www.bhphotovideo.com/c/product/1606948-REG/gigabyte_gv_n306teagle_oc_8gd_rtx_3060_ti_eagle.html'
 		},
-				{
+		{
 			brand: 'gigabyte',
 			model: 'eagle',
 			series: '3060ti',
-			url: 'https://www.bhphotovideo.com/c/product/1606949-REG/gigabyte_gv_n306teagle_8gd_rtx_3060_ti_eagle.html'
+			url:
+				'https://www.bhphotovideo.com/c/product/1606949-REG/gigabyte_gv_n306teagle_8gd_rtx_3060_ti_eagle.html'
 		},
 		{
 			brand: 'gigabyte',
 			model: 'aorus',
 			series: '3060ti',
-			url: 'https://www.bhphotovideo.com/c/product/1606945-REG/gigabyte_gv_n306taorus_m_8gd_aorus_rtx_3060_ti.html'
+			url:
+				'https://www.bhphotovideo.com/c/product/1606945-REG/gigabyte_gv_n306taorus_m_8gd_aorus_rtx_3060_ti.html'
 		},
 		{
 			brand: 'gigabyte',
 			model: 'gaming oc',
 			series: '3060ti',
-			url: 'https://www.bhphotovideo.com/c/product/1606947-REG/gigabyte_gv_n306tgaming_oc_8gd_rtx_3060_ti_gaming.html'
+			url:
+				'https://www.bhphotovideo.com/c/product/1606947-REG/gigabyte_gv_n306tgaming_oc_8gd_rtx_3060_ti_gaming.html'
 		},
 				{
 			brand: 'gigabyte',
 			model: 'gaming pro',
 			series: '3060ti',
-			url: 'https://www.bhphotovideo.com/c/product/1606946-REG/gigabyte_gv_n306tgamingoc_pro_8gd_rtx_3060_ti_gaming.html'
+			url:
+				'https://www.bhphotovideo.com/c/product/1606946-REG/gigabyte_gv_n306tgamingoc_pro_8gd_rtx_3060_ti_gaming.html'
 		},
 
 		{
