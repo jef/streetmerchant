ea456116da23d879557ecb071c8d041f84edbf92
Joseph Sortino <josephsortino@hotmail.com>
2020-12-02 13:28:35 -0500

    chore(bandh): add 3060ti (#1104)

--- src/store/model/bandh.ts
+++ src/store/model/bandh.ts
@@ -70,6 +70,42 @@ export const BAndH: Store = {
 		// 	url: 'https://www.bhphotovideo.com/c/product/1593650-REG/asus_tuf_rtx3080_o10g_gaming_tuf_gaming_geforce_rtx.html'
 		// },
 		{
+			brand: 'gigabyte',
+			model: 'eagle oc',
+			series: '3060ti',
+			url:
+				'https://www.bhphotovideo.com/c/product/1606948-REG/gigabyte_gv_n306teagle_oc_8gd_rtx_3060_ti_eagle.html'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle',
+			series: '3060ti',
+			url:
+				'https://www.bhphotovideo.com/c/product/1606949-REG/gigabyte_gv_n306teagle_8gd_rtx_3060_ti_eagle.html'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'aorus',
+			series: '3060ti',
+			url:
+				'https://www.bhphotovideo.com/c/product/1606945-REG/gigabyte_gv_n306taorus_m_8gd_aorus_rtx_3060_ti.html'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'gaming oc',
+			series: '3060ti',
+			url:
+				'https://www.bhphotovideo.com/c/product/1606947-REG/gigabyte_gv_n306tgaming_oc_8gd_rtx_3060_ti_gaming.html'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'gaming pro',
+			series: '3060ti',
+			url:
+				'https://www.bhphotovideo.com/c/product/1606946-REG/gigabyte_gv_n306tgamingoc_pro_8gd_rtx_3060_ti_gaming.html'
+		},
+
+		{
 			brand: 'msi',
 			model: 'gaming x trio',
 			series: '3080',
