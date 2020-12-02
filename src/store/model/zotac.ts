ef8d3b3b53ee65b854f0158dd51bc798c01cdbc3
Foxlet <foxlet@furcode.co>
2020-12-02 11:49:55 -0500

    chore(zotac): add 3070

--- src/store/model/zotac.ts
+++ src/store/model/zotac.ts
@@ -36,6 +36,20 @@ export const Zotac: Store = {
 		},
 		{
 			brand: 'zotac',
+			model: 'twin edge',
+			series: '3070',
+			url:
+				'https://store.zotac.com/zotac-gaming-geforce-rtx-3070-twin-edge-zt-a30700e-10p'
+		},
+		{
+			brand: 'zotac',
+			model: 'twin edge oc',
+			series: '3070',
+			url:
+				'https://store.zotac.com/zotac-gaming-geforce-rtx-3070-twin-edge-oc-zt-a30700h-10p'
+		},
+		{
+			brand: 'zotac',
 			model: 'trinity',
 			series: '3080',
 			url:
