0c8af1e7171a8ac7d4773e9a03239860cdee6d57
maglat <44977256+maglat@users.noreply.github.com>
2020-12-02 18:38:47 +0100

    Update euronics-de.ts
    Playstattion 5 added

--- src/store/model/euronics-de.ts
+++ src/store/model/euronics-de.ts
@@ -22,7 +22,19 @@ export const EuronicsDE: Store = {
 			series: 'xboxss',
 			url:
 				'https://www.euronics.de/spiele-und-konsolen-film-und-musik/spiele-und-konsolen/xbox-series-x/spielekonsole/xbox-series-s-512gb-konsole-4061856838076'
-		}
+		},
+                {
+                        brand: 'sony',
+                        model: 'ps5 console',
+                        series: 'sonyps5c',
+                        url: 'https://euronics.de/spiele-und-konsolen-film-und-musik/spiele-und-konsolen/playstation-5/spielekonsole/playstation-5-konsole-4061856837826'
+                },
+                {
+                        brand: 'sony',
+                        model: 'ps5 digital',
+                        series: 'sonyps5de',
+                        url: 'https://www.euronics.de/spiele-und-konsolen-film-und-musik/spiele-und-konsolen/playstation-5/spielekonsole/playstation-5-digital-edition-konsole-4061856837833'
+                }
 	],
 	name: 'euronics-de'
 };
