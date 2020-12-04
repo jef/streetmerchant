09f0236086c59b25ebe611041bf063d536e3cc3d
desigi <pnantel@gmail.com>
2020-12-04 17:21:20 -0500

    feat(store): add 3060ti to newegg-ca, canadacomputer, memoryexpress (#1172)
    Co-authored-by: Jef LeCompte <jeffreylec@gmail.com>

--- src/store/model/canadacomputers.ts
+++ src/store/model/canadacomputers.ts
@@ -356,6 +356,104 @@ export const CanadaComputers: Store = {
 				'https://www.canadacomputers.com/product_info.php?cPath=43_557_559&item_id=181352&language=en'
 		},
 		{
+			brand: 'msi',
+			model: 'gaming x trio',
+			series: '3060ti',
+			url:
+				'https://www.canadacomputers.com/product_info.php?cPath=43_557_559&item_id=185086&language=en'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf',
+			series: '3060ti',
+			url:
+				'https://www.canadacomputers.com/product_info.php?cPath=43_557_559&item_id=184759&language=en'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 2x oc',
+			series: '3060ti',
+			url:
+				'https://www.canadacomputers.com/product_info.php?cPath=43_557_559&item_id=185087&language=en'
+		},
+		{
+			brand: 'asus',
+			model: 'dual',
+			series: '3060ti',
+			url:
+				'https://www.canadacomputers.com/product_info.php?cPath=43_557_559&item_id=184760&language=en'
+		},
+		{
+			brand: 'zotac',
+			model: 'twin edge oc',
+			series: '3060ti',
+			url:
+				'https://www.canadacomputers.com/product_info.php?cPath=43_557_559&item_id=185751&language=en'
+		},
+		{
+			brand: 'zotac',
+			model: 'twin edge',
+			series: '3060ti',
+			url:
+				'https://www.canadacomputers.com/product_info.php?cPath=43_557_559&item_id=185752&language=en'
+		},
+		{
+			brand: 'asus',
+			model: 'strix',
+			series: '3060ti',
+			url:
+				'https://www.canadacomputers.com/product_info.php?cPath=43_557_559&item_id=184431&language=en'
+		},
+		{
+			brand: 'evga',
+			model: 'xc gaming',
+			series: '3060ti',
+			url:
+				'https://www.canadacomputers.com/product_info.php?cPath=43_557_559&item_id=185988&language=en'
+		},
+		{
+			brand: 'evga',
+			model: 'ftw3 ultra',
+			series: '3060ti',
+			url:
+				'https://www.canadacomputers.com/product_info.php?cPath=43_557_559&item_id=185987&language=en'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle oc',
+			series: '3060ti',
+			url:
+				'https://www.canadacomputers.com/product_info.php?cPath=43_557_559&item_id=185408&language=en'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'gaming oc',
+			series: '3060ti',
+			url:
+				'https://www.canadacomputers.com/product_info.php?cPath=43_557_559&item_id=185407&language=en'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'gaming oc pro',
+			series: '3060ti',
+			url:
+				'https://www.canadacomputers.com/product_info.php?cPath=43_557_559&item_id=185406&language=en'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'aorus',
+			series: '3060ti',
+			url:
+				'https://www.canadacomputers.com/product_info.php?cPath=43_557_559&item_id=185405&language=en'
+		},
+		{
+			brand: 'asus',
+			model: 'ko',
+			series: '3060ti',
+			url:
+				'https://www.canadacomputers.com/product_info.php?cPath=43_557_559&item_id=185168&language=en'
+		},
+		{
 			brand: 'amd',
 			model: '5600x',
 			series: 'ryzen5600',
