2b9ee2f58b255e05b49236e973ecc4f7d2c36dfd
desigi <pnantel@gmail.com>
2020-12-04 13:13:14 -0500

    added 3060ti models

--- src/store/model/memoryexpress.ts
+++ src/store/model/memoryexpress.ts
@@ -21,6 +21,84 @@ export const MemoryExpress: Store = {
 			url: 'https://www.memoryexpress.com/Products/MX79473'
 		},
 		{
+                        brand: 'msi',
+                        model: 'gaming x trio',
+                        series: '3060ti',
+                        url: 'https://www.memoryexpress.com/Products/MX00114969'
+                },
+		{
+                        brand: 'msi',
+                        model: 'ventus 2x oc',
+                        series: '3060ti',
+                        url: 'https://www.memoryexpress.com/Products/MX00114970'
+                },
+		{
+                        brand: 'asus',
+                        model: 'dual',
+                        series: '3060ti',
+                        url: 'https://www.memoryexpress.com/Products/MX00114818'
+                },
+		{
+                        brand: 'asus',
+                        model: 'strix',
+                        series: '3060ti',
+                        url: 'https://www.memoryexpress.com/Products/MX00114786'
+                },
+		{
+                        brand: 'asus',
+                        model: 'tuf',
+                        series: '3060ti',
+                        url: 'https://www.memoryexpress.com/Products/MX00114819'
+                },
+		{
+                        brand: 'gigabyte',
+                        model: 'aorus',
+                        series: '3060ti',
+                        url: 'https://www.memoryexpress.com/Products/MX00114923'
+                },
+		{
+                        brand: 'gigabyte',
+                        model: 'eagle',
+                        series: '3060ti',
+                        url: 'https://www.memoryexpress.com/Products/MX00114927'
+                },
+		{
+                        brand: 'gigabyte',
+                        model: 'eagle oc',
+                        series: '3060ti',
+                        url: 'https://www.memoryexpress.com/Products/MX00114926'
+                },
+		{
+                        brand: 'evga',
+                        model: 'ftw3 ultra',
+                        series: '3060ti',
+                        url: 'https://www.memoryexpress.com/Products/MX00115013'
+                },
+		{
+                        brand: 'gigabyte',
+                        model: 'gaming oc',
+                        series: '3060ti',
+                        url: 'https://www.memoryexpress.com/Products/MX00114925'
+                },
+		{
+                        brand: 'gigabyte',
+                        model: 'gaming oc pro',
+                        series: '3060ti',
+                        url: 'https://www.memoryexpress.com/Products/MX00114924'
+                },
+		{
+                        brand: 'evga',
+                        model: 'xc gaming',
+                        series: '3060ti',
+                        url: 'https://www.memoryexpress.com/Products/MX00115014'
+                },
+		{
+                        brand: 'asus',
+                        model: 'ko',
+                        series: '3060ti',
+                        url: 'https://www.memoryexpress.com/Products/MX00114888'
+                },
+		{
 			brand: 'amd',
 			model: '5950x',
 			series: 'ryzen5950',
