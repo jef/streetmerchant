b535b470cad38af034889b4d27578b20136e166d
Dylan Fontaine <10430358+dylanfontaine@users.noreply.github.com>
2020-12-08 08:35:00 -0800

    feat(store): add Walmart (CA) (#1253)

--- src/store/model/index.ts
+++ src/store/model/index.ts
@@ -81,6 +81,7 @@ import {Unieuro} from './unieuro';
 import {Very} from './very';
 import {VsGamers} from './vsgamers';
 import {Walmart} from './walmart';
+import {WalmartCa} from './walmart-ca';
 import {Wipoid} from './wipoid';
 import {Xbox} from './xbox';
 import {Zotac} from './zotac';
@@ -168,6 +169,7 @@ export const storeList = new Map([
 	[Very.name, Very],
 	[VsGamers.name, VsGamers],
 	[Walmart.name, Walmart],
+	[WalmartCa.name, WalmartCa],
 	[Wipoid.name, Wipoid],
 	[Xbox.name, Xbox],
 	[Zotac.name, Zotac]
