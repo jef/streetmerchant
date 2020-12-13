87b695ce7788770897fac83815f69f9603bd2599
Bailey Miller <baileymiller@live.com>
2020-12-13 17:38:59 -0500

    fixes: Ran prettier formatting

--- src/notification/redis.ts
+++ src/notification/redis.ts
@@ -3,41 +3,36 @@ import {config} from '../config';
 import {logger} from '../logger';
 import redis from 'redis';
 
-const {url} = config.notifications.redis;
+const { url } = config.notifications.redis;
 
 const client = redis.createClient({
-    url: url
+	url: url,
 });
 
 const updateRedis = (link: Link, store: Store) => {
+	try {
+		if (url) {
+			const key = `${store.name}:${link.brand}:${link.model}`;
 
-    try 
-    {
-        if (url)
-        {
-            const key = `${store.name}:${link.brand}:${link.model}`;
+			const value = {
+				...link,
+				labels: store.labels,
+				links: store.links,
+				name: store.name,
+				updatedAt: new Date().toUTCString(),
+			};
 
-            const value = {
-                ...link,
-                labels: store.labels,
-                links: store.links,
-                name: store.name,
-                updatedAt: new Date().toUTCString()
-            };
+			const redisUpdated = client.set(key, JSON.stringify(value));
 
-            const redisUpdated = client.set(key, JSON.stringify(value));
-
-            if (redisUpdated) {
-                logger.error("✖ couldn't update redis");
-            }
-            else {
-                logger.info('✔ redis updated');
-            }
-        }
-    }
-    catch (error) {
-        logger.error("✖ couldn't update redis", error);
-    }
-}
+			if (redisUpdated) {
+				logger.error("✖ couldn't update redis");
+			} else {
+				logger.info("✔ redis updated");
+			}
+		}
+	} catch (error) {
+		logger.error("✖ couldn't update redis", error);
+	}
+};
 
 export default updateRedis;
