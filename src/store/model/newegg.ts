30c6a8c5772bb419e7d36e1f9ba93f8c0059cb02
StanZha <stas.zhabinskiy@gmail.com>
2020-12-14 23:06:41 -0600

    more 6900xt added

--- src/store/model/newegg.ts
+++ src/store/model/newegg.ts
@@ -648,6 +648,24 @@ export const Newegg: Store = {
 				'https://www.newegg.com/sapphire-radeon-rx-6900-xt-21308-01-20g/p/N82E16814202393'
 		},
 		{
+			brand: 'asrock',
+			cartUrl:
+				'https://secure.newegg.com/Shopping/AddtoCart.aspx?Submit=ADD&ItemList=N82E16814930052',
+			model: 'phantom gaming',
+			series: 'rx6900xt',
+			url:
+				'https://www.newegg.com/asrock-radeon-rx-6800-xt-rx6900xt-pgd-16g/p/N82E16814930052'
+		},
+		{
+			brand: 'xfx',
+			cartUrl:
+				'https://secure.newegg.com/Shopping/AddtoCart.aspx?Submit=ADD&ItemList=N82E16814150846',
+			model: 'merc',
+			series: 'rx6900xt',
+			url:
+				'https://www.newegg.com/xfx-radeon-rx-6900-xt-rx-69xtacud9/p/N82E16814150846'
+		},
+		{
 			brand: 'gigabyte',
 			cartUrl:
 				'https://secure.newegg.com/Shopping/AddtoCart.aspx?Submit=ADD&ItemList=N82E16814932373',
