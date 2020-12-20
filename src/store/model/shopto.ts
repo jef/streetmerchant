09b6cec7f265a886ea83789b67bc20717cbd00d2
Jef LeCompte <jeffreylec@gmail.com>
2020-12-20 17:53:12 -0500

    chore: linting

--- src/store/model/shopto.ts
+++ src/store/model/shopto.ts
@@ -13,19 +13,22 @@ export const ShopTo: Store = {
 			brand: 'test:brand',
 			model: 'test:model',
 			series: 'test:series',
-			url: 'https://www.shopto.net/en/ps5du00-dualsense-controller-playstation-5-p195100/'
+			url:
+				'https://www.shopto.net/en/ps5du00-dualsense-controller-playstation-5-p195100/'
 		},
 		{
 			brand: 'sony',
 			model: 'ps5 console',
 			series: 'sonyps5c',
-			url: 'https://www.shopto.net/en/ps5hw01-playstation-5-console-p191472/'
+			url:
+				'https://www.shopto.net/en/ps5hw01-playstation-5-console-p191472/'
 		},
 		{
 			brand: 'sony',
 			model: 'ps5 digital',
 			series: 'sonyps5de',
-			url: 'https://www.shopto.net/en/ps5hw02-playstation-5-digital-console-p195341/'
+			url:
+				'https://www.shopto.net/en/ps5hw02-playstation-5-digital-console-p195341/'
 		}
 	],
 	name: 'shopto'
