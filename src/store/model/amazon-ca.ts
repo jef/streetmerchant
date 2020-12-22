28d1cf90b8fe3f92c1ed5850268cb367bbed2463
StanZha <stas.zhabinskiy@gmail.com>
2020-12-22 10:57:57 -0600

    chore(store): rx6800xt for memoryexpress, amazon-ca (#1491)
    * rx6900xt urls added for amd official store, newegg and bestbuy
    
    * added rx6900xt microcenter url
    
    * linter issue fix
    
    * few more 6800xt added for Canada

--- src/store/model/amazon-ca.ts
+++ src/store/model/amazon-ca.ts
@@ -271,6 +271,22 @@ export const AmazonCa: Store = {
 			model: 'xbox series s',
 			series: 'xboxss',
 			url: 'https://www.amazon.ca/dp/B08G9J44ZN'
+		},
+		{
+			brand: 'xfx',
+			cartUrl:
+				'https://www.amazon.ca/gp/aws/cart/add.html?ASIN.1=B08NX14LV1&Quantity.1=1',
+			model: 'merc',
+			series: 'rx6800xt',
+			url: 'https://www.amazon.ca/dp/B08NX14LV1'
+		},
+		{
+			brand: 'sapphire',
+			cartUrl:
+				'https://www.amazon.ca/gp/aws/cart/add.html?ASIN.1=B08PQ585CX&Quantity.1=1',
+			model: 'pulse',
+			series: 'rx6800xt',
+			url: 'https://www.amazon.ca/dp/B08PQ585CX'
 		}
 	],
 	name: 'amazon-ca'
