14998d87f99acf171ace85e1eb9b059d9e0a5b65
Dan Robinson <32175905+DanRobinsonHPE@users.noreply.github.com>
2020-12-25 16:24:04 -0800

    chore(evga): add 3060ti cards

--- src/store/model/evga.ts
+++ src/store/model/evga.ts
@@ -17,6 +17,24 @@ export const Evga: Store = {
 		},
 		{
 			brand: 'evga',
+			model: 'xc gaming',
+			series: '3060ti',
+			url: 'https://www.evga.com/products/product.aspx?pn=08G-P5-3663-KR'
+		},
+		{
+			brand: 'evga',
+			model: 'ftw3',
+			series: '3060ti',
+			url: 'https://www.evga.com/products/product.aspx?pn=08G-P5-3665-KR'
+		},
+		{
+			brand: 'evga',
+			model: 'ftw3 ultra',
+			series: '3060ti',
+			url: 'https://www.evga.com/products/product.aspx?pn=08G-P5-3667-KR'
+		},
+		{
+			brand: 'evga',
 			model: 'xc3 black',
 			series: '3070',
 			url: 'https://www.evga.com/products/product.aspx?pn=08G-P5-3751-KR'
