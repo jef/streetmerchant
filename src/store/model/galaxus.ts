dac3bc07712efa9b699798f6b6f267a1a156a169
wofnull <wofnull@gmail.com>
2021-01-03 08:11:48 +0100

    fix(galaxus): update selector (#1578)

--- src/store/model/galaxus.ts
+++ src/store/model/galaxus.ts
@@ -8,7 +8,7 @@ export const Galaxus: Store = {
 			text: ['In den Warenkorb']
 		},
 		maxPrice: {
-			container: '.productDetail .ZZ92',
+			container: '.productDetail .Z1ej',
 			euroFormat: true
 		}
 	},
