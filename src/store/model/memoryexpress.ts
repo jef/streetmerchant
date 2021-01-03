9126e8f7119f8f9a249374443f9b46d05acc251b
Anthony Tam <anthonytam@users.noreply.github.com>
2021-01-03 02:13:18 -0500

    fix(store): update missing/mislabeled cards for Canadian retailers (#1574)

--- src/store/model/memoryexpress.ts
+++ src/store/model/memoryexpress.ts
@@ -35,25 +35,25 @@ export const MemoryExpress: Store = {
 		},
 		{
 			brand: 'asus',
-			model: 'dual',
+			model: 'dual oc',
 			series: '3060ti',
 			url: 'https://www.memoryexpress.com/Products/MX00114818'
 		},
 		{
 			brand: 'asus',
-			model: 'strix',
+			model: 'strix oc',
 			series: '3060ti',
 			url: 'https://www.memoryexpress.com/Products/MX00114786'
 		},
 		{
 			brand: 'asus',
-			model: 'tuf',
+			model: 'tuf oc',
 			series: '3060ti',
 			url: 'https://www.memoryexpress.com/Products/MX00114819'
 		},
 		{
 			brand: 'gigabyte',
-			model: 'aorus',
+			model: 'aorus master',
 			series: '3060ti',
 			url: 'https://www.memoryexpress.com/Products/MX00114923'
 		},
@@ -124,12 +124,6 @@ export const MemoryExpress: Store = {
 			url: 'https://www.memoryexpress.com/Products/MX00114455'
 		},
 		{
-			brand: 'xfx',
-			model: 'amd reference',
-			series: 'rx6800xt',
-			url: 'https://www.memoryexpress.com/Products/MX00114996'
-		},
-		{
 			brand: 'gigabyte',
 			model: 'eagle',
 			series: '3080',
@@ -161,7 +155,7 @@ export const MemoryExpress: Store = {
 		},
 		{
 			brand: 'asus',
-			model: 'gaming oc',
+			model: 'tuf oc',
 			series: '3080',
 			url: 'https://www.memoryexpress.com/Products/MX00114003'
 		},
@@ -214,22 +208,257 @@ export const MemoryExpress: Store = {
 			url: 'https://www.memoryexpress.com/Products/MX00114404'
 		},
 		{
+			brand: 'asus',
+			model: 'strix oc',
+			series: '3080',
+			url: 'https://www.memoryexpress.com/Products/MX00115134'
+		},
+		{
+			brand: 'msi',
+			model: 'suprim x',
+			series: '3080',
+			url: 'https://www.memoryexpress.com/Products/MX00114907'
+		},
+		{
+			brand: 'asus',
+			model: 'dual oc',
+			series: '3070',
+			url: 'https://www.memoryexpress.com/Products/MX00114566'
+		},
+		{
+			brand: 'asus',
+			model: 'ko',
+			series: '3070',
+			url: 'https://www.memoryexpress.com/Products/MX00114785'
+		},
+		{
+			brand: 'asus',
+			model: 'strix oc',
+			series: '3070',
+			url: 'https://www.memoryexpress.com/Products/MX00114560'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf oc',
+			series: '3070',
+			url: 'https://www.memoryexpress.com/Products/MX00114567'
+		},
+		{
+			brand: 'evga',
+			model: 'ftw3 ultra',
+			series: '3070',
+			url: 'https://www.memoryexpress.com/Products/MX00114607'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3 black',
+			series: '3070',
+			url: 'https://www.memoryexpress.com/Products/MX00114605'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3 ultra',
+			series: '3070',
+			url: 'https://www.memoryexpress.com/Products/MX00114606'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'aorus master',
+			series: '3070',
+			url: 'https://www.memoryexpress.com/Products/MX00114688'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle oc',
+			series: '3070',
+			url: 'https://www.memoryexpress.com/Products/MX00114407'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'gaming oc',
+			series: '3070',
+			url: 'https://www.memoryexpress.com/Products/MX00114405'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'vision oc',
+			series: '3070',
+			url: 'https://www.memoryexpress.com/Products/MX00114689'
+		},
+		{
+			brand: 'msi',
+			model: 'gaming x trio',
+			series: '3070',
+			url: 'https://www.memoryexpress.com/Products/MX00114447'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 2x oc',
+			series: '3070',
+			url: 'https://www.memoryexpress.com/Products/MX00114448'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 3x oc',
+			series: '3070',
+			url: 'https://www.memoryexpress.com/Products/MX00114449'
+		},
+		// TODO: uncomment this when #1555 is merged
+		/* {
+			brand: 'asus',
+			model: 'ekwb',
+			series: '3090',
+			url: 'https://www.memoryexpress.com/Products/MX00115135'
+		}, */
+		{
+			brand: 'asus',
+			model: 'strix oc',
+			series: '3090',
+			url: 'https://www.memoryexpress.com/Products/MX00114093'
+		},
+		{
+			brand: 'asus',
+			model: 'strix oc',
+			series: '3090',
+			url: 'https://www.memoryexpress.com/Products/MX00115133'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf oc',
+			series: '3090',
+			url: 'https://www.memoryexpress.com/Products/MX00114001'
+		},
+		{
+			brand: 'evga',
+			model: 'ftw3',
+			series: '3090',
+			url: 'https://www.memoryexpress.com/Products/MX00114315'
+		},
+		{
+			brand: 'evga',
+			model: 'ftw3 ultra',
+			series: '3090',
+			url: 'https://www.memoryexpress.com/Products/MX00114155'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3',
+			series: '3090',
+			url: 'https://www.memoryexpress.com/Products/MX00114153'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3 ultra',
+			series: '3090',
+			url: 'https://www.memoryexpress.com/Products/MX00114154'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'aorus master',
+			series: '3090',
+			url: 'https://www.memoryexpress.com/Products/MX00114401'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'aorus xtreme',
+			series: '3090',
+			url: 'https://www.memoryexpress.com/Products/MX00114397'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle',
+			series: '3090',
+			url: 'https://www.memoryexpress.com/Products/MX00114686'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle oc',
+			series: '3090',
+			url: 'https://www.memoryexpress.com/Products/MX00113953'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'gaming oc',
+			series: '3090',
+			url: 'https://www.memoryexpress.com/Products/MX00113952'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'vision oc',
+			series: '3090',
+			url: 'https://www.memoryexpress.com/Products/MX00114685'
+		},
+		{
+			brand: 'msi',
+			model: 'gaming x trio',
+			series: '3090',
+			url: 'https://www.memoryexpress.com/Products/MX00113959'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 3x oc',
+			series: '3090',
+			url: 'https://www.memoryexpress.com/Products/MX00113958'
+		},
+		{
+			brand: 'msi',
+			model: 'suprim x',
+			series: '3090',
+			url: 'https://www.memoryexpress.com/Products/MX00114908'
+		},
+		{
 			brand: 'sapphire',
 			model: 'pulse',
 			series: 'rx6800xt',
-			url: 'https://www.memoryexpress.com/Products/MX00114404'
+			url: 'https://www.memoryexpress.com/Products/MX00115124'
 		},
 		{
 			brand: 'gigabyte',
 			model: 'gaming oc',
 			series: 'rx6800xt',
-			url: 'https://www.memoryexpress.com/Products/MX00114404'
+			url: 'https://www.memoryexpress.com/Products/MX00115048'
 		},
 		{
 			brand: 'xfx',
 			model: 'merc',
 			series: 'rx6800xt',
-			url: 'https://www.memoryexpress.com/Products/MX00114404'
+			url: 'https://www.memoryexpress.com/Products/MX00114996'
+		},
+		{
+			brand: 'asus',
+			model: 'strix oc',
+			series: 'rx6800',
+			url: 'https://www.memoryexpress.com/Products/MX00114938'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf oc',
+			series: 'rx6800',
+			url: 'https://www.memoryexpress.com/Products/MX00114937'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'gaming oc',
+			series: 'rx6800',
+			url: 'https://www.memoryexpress.com/Products/MX00115049'
+		},
+		{
+			brand: 'sapphire',
+			model: 'nitro+',
+			series: 'rx6800',
+			url: 'https://www.memoryexpress.com/Products/MX00115123'
+		},
+		{
+			brand: 'xfx',
+			model: 'merc',
+			series: 'rx6800',
+			url: 'https://www.memoryexpress.com/Products/MX00114997'
+		},
+		{
+			brand: 'xfx',
+			model: 'merc',
+			series: 'rx6900xt',
+			url: 'https://www.memoryexpress.com/Products/MX00115051'
 		}
 	],
 	name: 'memoryexpress',
