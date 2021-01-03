2d3ae4202ac711eaa56689bbf70ff78c832393a7
Staffan <enemark51@hotmail.com>
2021-01-03 08:03:11 +0100

    chore(proshop-dk): add 3060ti, add 3070, update 3080, update 3090, add ryzen cpus (#1526)

--- src/store/model/proshop-dk.ts
+++ src/store/model/proshop-dk.ts
@@ -22,155 +22,607 @@ export const ProshopDK: Store = {
 			brand: 'test:brand',
 			model: 'test:model',
 			series: 'test:series',
-			url:
-				'https://www.proshop.dk/Grafikkort/ASUS-GeForce-RTX-2060-TUF-OC-6GB-GDDR6-RAM-Grafikkort/2694767'
+			url: 'https://www.proshop.dk/2694767'
 		},
 		{
-			brand: 'asus',
-			model: 'tuf',
-			series: '3080',
-			url:
-				'https://www.proshop.dk/Grafikkort/ASUS-GeForce-RTX-3080-TUF-10GB-GDDR6X-RAM-Grafikkort/2876763'
+			brand: 'msi',
+			model: 'gaming x trio',
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2910054'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 3x oc',
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2910052'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 2x oc',
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2911560'
 		},
 		{
 			brand: 'asus',
 			model: 'tuf oc',
-			series: '3080',
-			url:
-				'https://www.proshop.dk/Grafikkort/ASUS-GeForce-RTX-3080-TUF-OC-10GB-GDDR6X-RAM-Grafikkort/2876861'
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2886986'
 		},
 		{
-			brand: 'asus',
-			model: 'strix',
-			series: '3080',
-			url:
-				'https://www.proshop.dk/Grafikkort/ASUS-GeForce-RTX-3080-ROG-STRIX-10GB-GDDR6X-RAM-Grafikkort/2876857'
+			brand: 'inno3d',
+			model: 'twin x2',
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2911556'
 		},
 		{
 			brand: 'asus',
 			model: 'strix oc',
-			series: '3080',
-			url:
-				'https://www.proshop.dk/Grafikkort/ASUS-GeForce-RTX-3080-ROG-STRIX-OC-10GB-GDDR6X-RAM-Grafikkort/2876859'
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2886982'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'aorus master',
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2887734'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle',
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2887735'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'gaming oc',
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2887737'
+		},
+		{
+			brand: 'asus',
+			model: 'dual',
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2886988'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'gaming oc pro',
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2887738'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle oc',
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2887736'
+		},
+		{
+			brand: 'inno3d',
+			model: 'ichill x3',
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2908274'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf',
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2886984'
+		},
+		{
+			brand: 'inno3d',
+			model: 'twin x2 oc',
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2908273'
+		},
+		{
+			brand: 'asus',
+			model: 'dual oc',
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2886990'
+		},
+		{
+			brand: 'asus',
+			model: 'dual mini',
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2914081'
+		},
+		{
+			brand: 'asus',
+			model: 'strix',
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2886980'
+		},
+		{
+			brand: 'asus',
+			model: 'dual mini oc',
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2914082'
+		},
+		{
+			brand: 'zotac',
+			model: 'twin edge',
+			series: '3060ti',
+			url: 'https://www.proshop.dk/2914133'
+		},
+		{
+			brand: 'msi',
+			model: 'gaming x trio',
+			series: '3070',
+			url: 'https://www.proshop.dk/2876871'
+		},
+		{
+			brand: 'msi',
+			model: 'suprim',
+			series: '3070',
+			url: 'https://www.proshop.dk/2908888'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 2x oc',
+			series: '3070',
+			url: 'https://www.proshop.dk/2876873'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 3x oc',
+			series: '3070',
+			url: 'https://www.proshop.dk/2876875'
+		},
+		{
+			brand: 'msi',
+			model: 'suprim x',
+			series: '3070',
+			url: 'https://www.proshop.dk/2885269'
+		},
+		{
+			brand: 'asus',
+			model: 'strix oc',
+			series: '3070',
+			url: 'https://www.proshop.dk/2876845'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf oc',
+			series: '3070',
+			url: 'https://www.proshop.dk/2876856'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'gaming oc',
+			series: '3070',
+			url: 'https://www.proshop.dk/2878390'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'aorus master',
+			series: '3070',
+			url: 'https://www.proshop.dk/2878383'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'vision oc',
+			series: '3070',
+			url: 'https://www.proshop.dk/2878392'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle oc',
+			series: '3070',
+			url: 'https://www.proshop.dk/2878386'
+		},
+		{
+			brand: 'asus',
+			model: 'dual oc',
+			series: '3070',
+			url: 'https://www.proshop.dk/2876853'
+		},
+		{
+			brand: 'asus',
+			model: 'dual',
+			series: '3070',
+			url: 'https://www.proshop.dk/2876851'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf',
+			series: '3070',
+			url: 'https://www.proshop.dk/2876854'
+		},
+		{
+			brand: 'inno3d',
+			model: 'twin x2 oc',
+			series: '3070',
+			url: 'https://www.proshop.dk/2883917'
+		},
+		{
+			brand: 'asus',
+			model: 'strix',
+			series: '3070',
+			url: 'https://www.proshop.dk/2876843'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle',
+			series: '3070',
+			url: 'https://www.proshop.dk/2878385'
+		},
+		{
+			brand: 'asus',
+			model: 'strix oc',
+			series: '3070',
+			url: 'https://www.proshop.dk/2911575'
+		},
+		{
+			brand: 'inno3d',
+			model: 'twin x2',
+			series: '3070',
+			url: 'https://www.proshop.dk/2883916'
+		},
+		{
+			brand: 'inno3d',
+			model: 'ichill x4',
+			series: '3070',
+			url: 'https://www.proshop.dk/2883919'
+		},
+		{
+			brand: 'asus',
+			model: 'strix',
+			series: '3070',
+			url: 'https://www.proshop.dk/2911574'
+		},
+		{
+			brand: 'asus',
+			model: 'ekwb',
+			series: '3070',
+			url: 'https://www.proshop.dk/2911577'
+		},
+		{
+			brand: 'inno3d',
+			model: 'ichill x3',
+			series: '3070',
+			url: 'https://www.proshop.dk/2883918'
+		},
+		{
+			brand: 'zotac',
+			model: 'twin edge',
+			series: '3070',
+			url: 'https://www.proshop.dk/2911956'
+		},
+		{
+			brand: 'zotac',
+			model: 'amp holo',
+			series: '3070',
+			url: 'https://www.proshop.dk/2912456'
+		},
+		{
+			brand: 'palit',
+			model: 'gamerock oc',
+			series: '3070',
+			url: 'https://www.proshop.dk/2914382'
+		},
+		{
+			brand: 'palit',
+			model: 'gamerock oc',
+			series: '3070',
+			url: 'https://www.proshop.dk/2914381'
+		},
+		{
+			brand: 'asus',
+			model: 'strix',
+			series: '3070',
+			url: 'https://www.proshop.dk/2912910'
 		},
 		{
 			brand: 'msi',
 			model: 'gaming x trio',
 			series: '3080',
-			url:
-				'https://www.proshop.dk/Grafikkort/MSI-GeForce-RTX-3080-GAMING-X-TRIO-10GB-GDDR6X-RAM-Grafikkort/2876877'
+			url: 'https://www.proshop.dk/2876877'
+		},
+		{
+			brand: 'msi',
+			model: 'suprim x',
+			series: '3080',
+			url: 'https://www.proshop.dk/2885270'
 		},
 		{
 			brand: 'msi',
 			model: 'ventus 3x',
 			series: '3080',
-			url:
-				'https://www.proshop.dk/Grafikkort/MSI-GeForce-RTX-3080-Ventus-3X-10GB-GDDR6X-RAM-Grafikkort/2876878'
+			url: 'https://www.proshop.dk/2876878'
 		},
 		{
 			brand: 'msi',
 			model: 'ventus 3x oc',
 			series: '3080',
-			url:
-				'https://www.proshop.dk/Grafikkort/MSI-GeForce-RTX-3080-Ventus-3X-OC-10GB-GDDR6X-RAM-Grafikkort/2876879'
+			url: 'https://www.proshop.dk/2876879'
 		},
 		{
-			brand: 'inno3d',
-			model: 'ichill x3',
+			brand: 'msi',
+			model: 'gaming trio',
 			series: '3080',
-			url:
-				'https://www.proshop.dk/Grafikkort/Inno3D-GeForce-RTX-3080-iCHILL-X4-10GB-GDDR6X-SDRAM-Grafikkort/2878971'
+			url: 'https://www.proshop.dk/2876876'
 		},
 		{
-			brand: 'inno3d',
-			model: 'ichill x3',
+			brand: 'asus',
+			model: 'strix oc',
 			series: '3080',
-			url:
-				'https://www.proshop.dk/Grafikkort/Inno3D-GeForce-RTX-3080-iCHILL-X3-10GB-GDDR6X-SDRAM-Grafikkort/2878969'
+			url: 'https://www.proshop.dk/2876859'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf',
+			series: '3080',
+			url: 'https://www.proshop.dk/2876763'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf oc',
+			series: '3080',
+			url: 'https://www.proshop.dk/2876861'
+		},
+		{
+			brand: 'asus',
+			model: 'strix oc',
+			series: '3080',
+			url: 'https://www.proshop.dk/2911579'
 		},
 		{
 			brand: 'gigabyte',
 			model: 'gaming oc',
 			series: '3080',
-			url:
-				'https://www.proshop.dk/Grafikkort/GIGABYTE-GeForce-RTX-3080-GAMING-OC-10GB-GDDR6X-RAM-Grafikkort/2876838'
-		},
-		{
-			brand: 'gigabyte',
-			model: 'eagle oc',
-			series: '3080',
-			url:
-				'https://www.proshop.dk/Grafikkort/GIGABYTE-GeForce-RTX-3080-Eagle-OC-10GB-GDDR6X-RAM-Grafikkort/2876837'
-		},
-		{
-			brand: 'inno3d',
-			model: 'twin x2 oc',
-			series: '3080',
-			url:
-				'https://www.proshop.dk/Grafikkort/Inno3D-GeForce-RTX-3080-Twin-X2-OC-10GB-GDDR6X-SDRAM-Grafikkort/2878968'
+			url: 'https://www.proshop.dk/2876838'
 		},
 		{
 			brand: 'gigabyte',
 			model: 'vision oc',
 			series: '3080',
-			url:
-				'https://www.proshop.dk/Grafikkort/GIGABYTE-GeForce-RTX-3080-Vision-OC-10GB-GDDR6X-RAM-Grafikkort/2878401'
+			url: 'https://www.proshop.dk/2878401'
 		},
 		{
 			brand: 'gigabyte',
 			model: 'aorus master',
 			series: '3080',
-			url:
-				'https://www.proshop.dk/Grafikkort/GIGABYTE-GeForce-RTX-3080-AORUS-Master-10GB-GDDR6X-RAM-Grafikkort/2876835'
-		},
-		{
-			brand: 'asus',
-			model: 'tuf',
-			series: '3090',
-			url:
-				'https://www.proshop.dk/Grafikkort/ASUS-GeForce-RTX-3090-TUF-24GB-GDDR6X-RAM-Grafikkort/2876764'
-		},
-		{
-			brand: 'asus',
-			model: 'tuf oc',
-			series: '3090',
-			url:
-				'https://www.proshop.dk/Grafikkort/ASUS-GeForce-RTX-3090-TUF-OC-24GB-GDDR6X-RAM-Grafikkort/2876869'
+			url: 'https://www.proshop.dk/2876835'
 		},
 		{
 			brand: 'asus',
 			model: 'strix',
-			series: '3090',
-			url:
-				'https://www.proshop.dk/Grafikkort/ASUS-GeForce-RTX-3090-ROG-STRIX-24GB-GDDR6X-RAM-Grafikkort/2876865'
+			series: '3080',
+			url: 'https://www.proshop.dk/2876857'
 		},
 		{
 			brand: 'asus',
-			model: 'strix oc',
-			series: '3090',
-			url:
-				'https://www.proshop.dk/Grafikkort/ASUS-GeForce-RTX-3090-ROG-STRIX-OC-24GB-GDDR6X-RAM-Grafikkort/2876867'
+			model: 'ekwb',
+			series: '3080',
+			url: 'https://www.proshop.dk/2887568'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle',
+			series: '3080',
+			url: 'https://www.proshop.dk/2878395'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'aorus xtreme',
+			series: '3080',
+			url: 'https://www.proshop.dk/2876836'
+		},
+		{
+			brand: 'inno3d',
+			model: 'ichill x4',
+			series: '3080',
+			url: 'https://www.proshop.dk/2878971'
+		},
+		{
+			brand: 'inno3d',
+			model: 'twin x2 oc',
+			series: '3080',
+			url: 'https://www.proshop.dk/2878968'
+		},
+		{
+			brand: 'asus',
+			model: 'strix',
+			series: '3080',
+			url: 'https://www.proshop.dk/2911578'
+		},
+		{
+			brand: 'inno3d',
+			model: 'ichill x3',
+			series: '3080',
+			url: 'https://www.proshop.dk/2878969'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle oc',
+			series: '3080',
+			url: 'https://www.proshop.dk/2876837'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'aorus xtreme waterforce',
+			series: '3080',
+			url: 'https://www.proshop.dk/2878393'
+		},
+		{
+			brand: 'pny',
+			model: 'xlr8 uprising',
+			series: '3080',
+			url: 'https://www.proshop.dk/2877066'
+		},
+		{
+			brand: 'pny',
+			model: 'xlr8 revel',
+			series: '3080',
+			url: 'https://www.proshop.dk/2877067'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'aorus xtreme waterforce wb',
+			series: '3080',
+			url: 'https://www.proshop.dk/2878394'
 		},
 		{
 			brand: 'msi',
 			model: 'gaming x trio',
 			series: '3090',
-			url:
-				'https://www.proshop.dk/Grafikkort/MSI-GeForce-RTX-3090-GAMING-X-TRIO-24GB-GDDR6X-RAM-Grafikkort/2876881'
+			url: 'https://www.proshop.dk/2876881'
+		},
+		{
+			brand: 'msi',
+			model: 'suprim x',
+			series: '3090',
+			url: 'https://www.proshop.dk/2885271'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 3x oc',
+			series: '3090',
+			url: 'https://www.proshop.dk/2876883'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 3x',
+			series: '3090',
+			url: 'https://www.proshop.dk/2876882'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf oc',
+			series: '3090',
+			url: 'https://www.proshop.dk/2876869'
+		},
+		{
+			brand: 'msi',
+			model: 'gaming trio',
+			series: '3090',
+			url: 'https://www.proshop.dk/2876880'
+		},
+		{
+			brand: 'inno3d',
+			model: 'gaming x3',
+			series: '3090',
+			url: 'https://www.proshop.dk/2878977'
+		},
+		{
+			brand: 'inno3d',
+			model: 'ichill x3',
+			series: '3090',
+			url: 'https://www.proshop.dk/2878978'
+		},
+		{
+			brand: 'inno3d',
+			model: 'ichill x4',
+			series: '3090',
+			url: 'https://www.proshop.dk/2878979'
+		},
+		{
+			brand: 'asus',
+			model: 'strix oc',
+			series: '3090',
+			url: 'https://www.proshop.dk/2876867'
 		},
 		{
 			brand: 'gigabyte',
 			model: 'aorus master',
 			series: '3090',
-			url:
-				'https://www.proshop.dk/Grafikkort/GIGABYTE-GeForce-RTX-3090-AORUS-Master-24GB-GDDR6X-RAM-Grafikkort/2876840'
+			url: 'https://www.proshop.dk/2876840'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf',
+			series: '3090',
+			url: 'https://www.proshop.dk/2876764'
+		},
+		{
+			brand: 'asus',
+			model: 'strix oc',
+			series: '3090',
+			url: 'https://www.proshop.dk/2911582'
+		},
+		{
+			brand: 'asus',
+			model: 'ekwb',
+			series: '3090',
+			url: 'https://www.proshop.dk/2887569'
 		},
 		{
 			brand: 'gigabyte',
 			model: 'aorus xtreme',
 			series: '3090',
-			url:
-				'https://www.proshop.dk/Grafikkort/GIGABYTE-GeForce-RTX-3090-AORUS-Xtreme-24GB-GDDR6X-RAM-Grafikkort/2876839'
+			url: 'https://www.proshop.dk/2876839'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'aorus xtreme waterforce',
+			series: '3090',
+			url: 'https://www.proshop.dk/2878404'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'vision oc',
+			series: '3090',
+			url: 'https://www.proshop.dk/2878413'
+		},
+		{
+			brand: 'inno3d',
+			model: 'ichill frostbite',
+			series: '3090',
+			url: 'https://www.proshop.dk/2887323'
+		},
+		{
+			brand: 'asus',
+			model: 'strix',
+			series: '3090',
+			url: 'https://www.proshop.dk/2878405'
+		},
+		{
+			brand: 'asus',
+			model: 'strix',
+			series: '3090',
+			url: 'https://www.proshop.dk/2911581'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle oc',
+			series: '3090',
+			url: 'https://www.proshop.dk/2876841'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'turbo',
+			series: '3090',
+			url: 'https://www.proshop.dk/2878410'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle',
+			series: '3090',
+			url: 'https://www.proshop.dk/2878406'
+		},
+		{
+			brand: 'palit',
+			model: 'gamerock oc',
+			series: '3090',
+			url: 'https://www.proshop.dk/2911898'
+		},
+		{
+			brand: 'amd',
+			model: '5800x',
+			series: 'ryzen5800',
+			url: 'https://www.proshop.dk/2884171'
+		},
+		{
+			brand: 'amd',
+			model: '5900x',
+			series: 'ryzen5900',
+			url: 'https://www.proshop.dk/2884173'
+		},
+		{
+			brand: 'amd',
+			model: '5950x',
+			series: 'ryzen5950',
+			url: 'https://www.proshop.dk/2884175'
 		}
 	],
 	name: 'proshop-dk'
