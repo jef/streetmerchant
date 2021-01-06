d19b6d6b046851965cdb3ce1123e699d229bf350
WoFNuLL <wofnull@gmail.com>
2021-01-06 17:45:55 +0100

    Galaxus changed the update selector the second time today: now it is Z1f2
    This change is aswell tested already

--- src/store/model/galaxus.ts
+++ src/store/model/galaxus.ts
@@ -8,7 +8,7 @@ export const Galaxus: Store = {
 			text: ['In den Warenkorb']
 		},
 		maxPrice: {
-			container: '.productDetail .Z1fp',
+			container: '.productDetail .Z1f2',
 			euroFormat: true
 		}
 	},
