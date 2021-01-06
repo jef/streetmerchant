859734a3b9dc98d0c6d73ddbc8a7ef72a0f22156
Paul Baum <pbaum83@gmail.com>
2021-01-06 09:17:03 -0600

    Updating to fix lint errors

--- src/config.ts
+++ src/config.ts
@@ -3,18 +3,20 @@ import {banner} from './banner';
 import dotenv from 'dotenv';
 import path from 'path';
 
-if( process.env.npm_config_conf ) {
-    if (existsSync(path.resolve(__dirname, '../' + process.env.npm_config_conf))) {
-        dotenv.config({path: path.resolve(__dirname, '../' + process.env.npm_config_conf)});
-    } else {
-        dotenv.config({path: path.resolve(__dirname, '../.env')});
-    }
+if (process.env.npm_config_conf) {
+	if (
+		existsSync(path.resolve(__dirname, '../' + process.env.npm_config_conf))
+	) {
+		dotenv.config({
+			path: path.resolve(__dirname, '../' + process.env.npm_config_conf)
+		});
+	} else {
+		dotenv.config({path: path.resolve(__dirname, '../.env')});
+	}
+} else if (existsSync(path.resolve(__dirname, '../dotenv'))) {
+	dotenv.config({path: path.resolve(__dirname, '../dotenv')});
 } else {
-    if (existsSync(path.resolve(__dirname, '../dotenv'))) {
-        dotenv.config({path: path.resolve(__dirname, '../dotenv')});
-    } else {
-        dotenv.config({path: path.resolve(__dirname, '../.env')});
-    }
+	dotenv.config({path: path.resolve(__dirname, '../.env')});
 }
 
 console.info(
