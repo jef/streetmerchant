7408b5edfcfb2189eb832ab6940f6f9cf4748867
WoFNuLL <wofnull@gmail.com>
2021-01-11 18:01:11 +0100

    Change update selector to Z1et ( change today 15:00 GMT ) -> Verified and tested. Awaited to work until wednesday.

--- src/store/model/galaxus.ts
+++ src/store/model/galaxus.ts
@@ -8,7 +8,7 @@ export const Galaxus: Store = {
 			text: ['In den Warenkorb']
 		},
 		maxPrice: {
-			container: '.productDetail .Z1fe',
+			container: '.productDetail .Z1et',
 			euroFormat: true
 		},
 		outOfStock: [
