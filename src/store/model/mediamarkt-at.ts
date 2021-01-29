f2546728e2dc9d65f0c7173f6e49e1e306810efb
Manuel Reiländer <manuel.reilaender@sonorys.at>
2021-01-29 22:44:35 +0100

    fix linting

--- src/store/model/mediamarkt-at.ts
+++ src/store/model/mediamarkt-at.ts
@@ -46,44 +46,51 @@ export const MediamarktAt: Store = {
       brand: 'gainward',
       model: 'phoenix',
       series: '3060ti',
-      url: 'https://www.mediamarkt.at/de/product/_gainward-grafikkarte-geforce-rtx-3060-ti-phoenix-golden-sample-8gb-ga-3060rtx-ti-ph-gs-1815563.html',
+      url:
+        'https://www.mediamarkt.at/de/product/_gainward-grafikkarte-geforce-rtx-3060-ti-phoenix-golden-sample-8gb-ga-3060rtx-ti-ph-gs-1815563.html',
     },
     {
       brand: 'gainward',
       model: 'phantom gaming',
       series: '3080',
-      url: 'https://www.mediamarkt.at/de/product/_gainward-grafikkarte-geforce-rtx-3080-phantom-10gb-ga-3080rtx-ph-1817678.html',
+      url:
+        'https://www.mediamarkt.at/de/product/_gainward-grafikkarte-geforce-rtx-3080-phantom-10gb-ga-3080rtx-ph-1817678.html',
     },
     {
       brand: 'asus',
       model: 'dual',
       series: '3060ti',
-      url: 'https://www.mediamarkt.at/de/product/_asus-grafikkarte-tuf-gaming-geforce-rtx-3060-ti-oc-8gb-tuf-rtx3060ti-o8g-gaming-1812392.html',
+      url:
+        'https://www.mediamarkt.at/de/product/_asus-grafikkarte-tuf-gaming-geforce-rtx-3060-ti-oc-8gb-tuf-rtx3060ti-o8g-gaming-1812392.html',
     },
     {
       brand: 'zotac',
       model: 'trinity',
       series: '3080',
-      url: 'https://www.mediamarkt.at/de/product/_zotac-grafikkarte-geforce-rtx-3080-trinity-oc-10gb-zt-a30800j-10p-1803318.html',
+      url:
+        'https://www.mediamarkt.at/de/product/_zotac-grafikkarte-geforce-rtx-3080-trinity-oc-10gb-zt-a30800j-10p-1803318.html',
     },
     {
       brand: 'asus',
       model: 'tuf',
       series: '3080',
-      url: 'https://www.mediamarkt.at/de/product/_asus-grafikkarte-tuf-gaming-geforce%C2%AE-rtx-3080-tuf-rtx3080-10g-gaming-10gb-gddr6x-90yv0fb0-m0nm00-1799192.html',
+      url:
+        'https://www.mediamarkt.at/de/product/_asus-grafikkarte-tuf-gaming-geforce%C2%AE-rtx-3080-tuf-rtx3080-10g-gaming-10gb-gddr6x-90yv0fb0-m0nm00-1799192.html',
     },
     {
       brand: 'msi',
       model: 'ventus 2x',
       series: '3070',
-      url: 'https://www.mediamarkt.at/de/product/_msi-grafikkarte-geforce-rtx%E2%84%A2-3070-ventus-2x-oc-8gb-v390-008r-1812232.html',
+      url:
+        'https://www.mediamarkt.at/de/product/_msi-grafikkarte-geforce-rtx%E2%84%A2-3070-ventus-2x-oc-8gb-v390-008r-1812232.html',
     },
     {
       brand: 'msi',
       model: 'gaming x trio',
       series: '3070',
-      url: 'https://www.mediamarkt.at/de/product/_msi-grafikkarte-geforce-rtx%E2%84%A2-3070-gaming-x-trio-8gb-v390-006r-1812223.html',
-    }
+      url:
+        'https://www.mediamarkt.at/de/product/_msi-grafikkarte-geforce-rtx%E2%84%A2-3070-gaming-x-trio-8gb-v390-006r-1812223.html',
+    },
   ],
   name: 'mediamarkt-at',
 };
