5e8a94f16e7b1ee33928a8c6cc743d50e7746fec
WoFNuLL <wofnull@gmail.com>
2021-01-31 19:17:43 +0100

    Galaxus patch for update selector set to Z1bp on 31.01.2021 - 19:00 GMT+1

--- src/store/model/galaxus.ts
+++ src/store/model/galaxus.ts
@@ -8,7 +8,7 @@ export const Galaxus: Store = {
       text: ['In den Warenkorb'],
     },
     maxPrice: {
-      container: '.productDetail .Z1c8',
+      container: '.productDetail .Z1bp',
       euroFormat: true,
     },
     outOfStock: [
