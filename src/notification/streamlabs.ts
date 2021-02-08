bd7fbef9c65d646b02dd132252020f185932ee40
TomZanna <43095563+TomZanna@users.noreply.github.com>
2021-02-08 08:40:52 +0100

    Switch to URLSearchParams

--- src/notification/streamlabs.ts
+++ src/notification/streamlabs.ts
@@ -1,35 +1,37 @@
 import {Link, Store} from '../store/model';
 import {Print, logger} from '../logger';
 import {config} from '../config';
-import FormData from 'form-data';
+import {URLSearchParams} from 'url';
 import fetch from 'node-fetch';
 
 const {streamlabs} = config.notifications;
+let requestParams: URLSearchParams;
 
-const formdata = new FormData();
-formdata.append('access_token', streamlabs.accessToken);
-formdata.append('type', streamlabs.type);
-formdata.append('image_href', streamlabs.imageHref);
-formdata.append('sound_href', streamlabs.soundHref);
-formdata.append('duration', streamlabs.duration.toString());
+if (streamlabs.accessToken) {
+  requestParams = new URLSearchParams();
+  requestParams.append('access_token', streamlabs.accessToken);
+  requestParams.append('type', streamlabs.type);
+  requestParams.append('image_href', streamlabs.imageHref);
+  requestParams.append('sound_href', streamlabs.soundHref);
+  requestParams.append('duration', streamlabs.duration.toString());
+}
 
 export function sendStreamLabsAlert(link: Link, store: Store) {
-  if (streamlabs.accessToken) {
+  if (requestParams) {
     logger.debug('↗ sending StreamLabs alert');
 
     (async () => {
       const message = `${Print.inStock(link, store)}`;
-      formdata.append('message', message);
+      requestParams.set('message', message);
 
       try {
         const response = await fetch('https://streamlabs.com/api/v1.0/alerts', {
           method: 'POST',
-            body: formdata,
-          }
-        );
+          body: requestParams,
+        });
 
         const json = await response.json();
-        if (!json.success) throw Error(status);
+        if (!json.success) throw Error(JSON.stringify(json));
 
         logger.info('✔ StreamLabs alert sent');
       } catch (error: unknown) {
