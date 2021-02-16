15b234dfd7caa6c2818cec24212b2b8a7dba9151
Epxiahs <79109710+Epxiahs@users.noreply.github.com>
2021-02-16 08:31:26 +0100

    Add files via upload

---
+++ mediamarktES.ts
@@ -0,0 +1,35 @@
+import {Store} from './store';
+
+export const MediamarktES: Store = {
+  currency: '€',
+  labels: {
+    inStock: {
+      container: '#pdp-add-to-cart',
+      text: ['Comprar'],
+    },
+    maxPrice: {
+      container: '.text-price-total',
+      euroFormat: true,
+    },
+    outOfStock: {
+      container: '.product-availability',
+      text: ['Producto Agotado'],
+    },
+  },
+  links: [
+    {
+      brand: 'test:brand',
+      model: 'test:model',
+      series: 'test:series',
+      url:
+        'https://www.mediamarkt.es/es/product/_consola-sony-ps5-825-gb-4k-hdr-blanco-1487016.html',
+    },
+    {
+      brand: 'sony',
+      model: 'ps5 console',
+      series: 'sonyps5c',
+      url:
+        'https://www.mediamarkt.es/es/product/_consola-sony-ps5-825-gb-4k-hdr-blanco-1487016.html' },
+  ],
+  name: 'mediamarktES',
+};
