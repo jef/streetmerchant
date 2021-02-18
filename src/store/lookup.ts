8d5792bf9e377f10d2c156923162d339ef356fc8
Jef LeCompte <jeffreylec@gmail.com>
2021-02-18 09:52:26 -0500

    chore: update dependencies
    Removed puppeteer types and puppeteer-extra

--- src/store/lookup.ts
+++ src/store/lookup.ts
@@ -1,10 +1,9 @@
 import {
   Browser,
   Page,
-  PageEventObj,
-  Request,
-  RespondOptions,
-  Response,
+  HTTPRequest,
+  HTTPResponse,
+  ResponseForRequest,
 } from 'puppeteer';
 import {Link, Store, getStores} from './model';
 import {Print, logger} from '../logger';
@@ -54,7 +53,7 @@ function nextProxy(store: Store) {
   return store.proxyList[store.currentProxyIndex];
 }
 
-async function handleLowBandwidth(request: Request) {
+async function handleLowBandwidth(request: HTTPRequest) {
   if (!config.browser.lowBandwidth) {
     return false;
   }
@@ -73,7 +72,7 @@ async function handleLowBandwidth(request: Request) {
   return false;
 }
 
-async function handleProxy(request: Request, proxy?: string) {
+async function handleProxy(request: HTTPRequest, proxy?: string) {
   if (!proxy) {
     return false;
   }
@@ -92,7 +91,7 @@ async function handleProxy(request: Request, proxy?: string) {
   return true;
 }
 
-async function handleAdBlock(request: Request, adBlockRequestHandler: any) {
+async function handleAdBlock(request: HTTPRequest, adBlockRequestHandler: any) {
   if (!adBlockRequestHandler) {
     return false;
   }
@@ -112,7 +111,7 @@ async function handleAdBlock(request: Request, adBlockRequestHandler: any) {
       resolve(true);
     };
 
-    const respondFunc = async (response: RespondOptions) => {
+    const respondFunc = async (response: ResponseForRequest) => {
       try {
         await request.respond(response);
       } catch {
@@ -199,7 +198,7 @@ async function lookup(browser: Browser, store: Store) {
     let adBlockRequestHandler: any;
     let pageProxy;
     if (useAdBlock) {
-      const onProxyFunc = (event: keyof PageEventObj, handler: any) => {
+      const onProxyFunc = (event: string, handler: any) => {
         if (event !== 'request') {
           page.on(event, handler);
           return;
@@ -298,7 +297,7 @@ async function lookupCard(
   link: Link
 ): Promise<number> {
   const givenWaitFor = store.waitUntil ? store.waitUntil : 'networkidle0';
-  const response: Response | null = await page.goto(link.url, {
+  const response: HTTPResponse | null = await page.goto(link.url, {
     waitUntil: givenWaitFor,
   });
 
@@ -347,7 +346,7 @@ async function handleResponse(
   store: Store,
   page: Page,
   link: Link,
-  response?: Response | null,
+  response?: HTTPResponse | null,
   recursionDepth = 0
 ) {
   if (!response) {
@@ -364,7 +363,7 @@ async function handleResponse(
         if (recursionDepth > 4) {
           logger.warn(Print.recursionLimit(link, store, true));
         } else {
-          const response: Response | null = await page.waitForNavigation({
+          const response: HTTPResponse | null = await page.waitForNavigation({
             waitUntil: 'networkidle0',
           });
           recursionDepth++;
@@ -522,7 +521,7 @@ async function runCaptchaDeterrent(browser: Browser, store: Store, page: Page) {
 
     try {
       const givenWaitFor = store.waitUntil ? store.waitUntil : 'networkidle0';
-      const response: Response | null = await page.goto(link.url, {
+      const response: HTTPResponse | null = await page.goto(link.url, {
         waitUntil: givenWaitFor,
       });
       statusCode = await handleResponse(browser, store, page, link, response);
