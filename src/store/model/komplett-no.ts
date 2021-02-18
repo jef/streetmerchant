5a5927ba1ef5d138f5741b672b1fe2466e5760c2
david <david@davido.me>
2021-02-18 15:53:57 +0100

    feat(store): add Norwegian stores (#1985)
    Adds elkjop.no, komplett.no, dustinhome.no, power.no and proshop.no

---
+++ src/store/model/komplett-no.ts
@@ -0,0 +1,261 @@
+import {Store} from './store';
+
+export const KomplettNO: Store = {
+  currency: 'kr.',
+  labels: {
+    inStock: {
+      container:
+        '#MainContent > div.no-bs-center.maincontent-container.container.main-body.ignore-gutter-xs.product-page-boxes > div.responsive-content-wrapper > div.product-page > section > div > section > div.product-main-info__body > div.product-main-info__buy-and-more > div.buy-button-section > div > div > div.product-main-info-buy-button > div.buy-button > button',
+      text: ['LEGG I HANDLEVOGN'],
+    },
+    outOfStock: {
+      container:
+        '#MainContent > div.no-bs-center.maincontent-container.container.main-body.ignore-gutter-xs.product-page-boxes > div.responsive-content-wrapper > div.product-page > section > div > section > div.product-main-info__body > div.product-main-info__buy-and-more > div.buy-button-section > div > div > div.subscribe-product-container > div > div > button',
+      text: ['MOTTA VARSEL'],
+    },
+  },
+  links: [
+    {
+      brand: 'test:brand',
+      model: 'test:model',
+      series: 'test:series',
+      url: 'https://www.komplett.no/product/1168415/',
+    },
+    {
+      brand: 'msi',
+      model: 'gaming x trio',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1168380/',
+    },
+    {
+      brand: 'msi',
+      model: 'suprim x',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1173669',
+    },
+    {
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1168379/',
+    },
+    {
+      brand: 'asus',
+      model: 'strix',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1168439/',
+    },
+    {
+      brand: 'asus',
+      model: 'tuf',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1168358/',
+    },
+    {
+      brand: 'asus',
+      model: 'strix oc',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1168436/',
+    },
+    {
+      brand: 'asus',
+      model: 'strix oc',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1176247/',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'gaming oc',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1168419',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'vision oc',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1171562',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'aorus master',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1168418',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'eagle',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1175718',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'eagle oc',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1168415',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'aorus xtreme',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1168416',
+    },
+    {
+      brand: 'gainward',
+      model: 'phantom gs',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1171801',
+    },
+    {
+      brand: 'gainward',
+      model: 'phantom gs',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1171798',
+    },
+    {
+      brand: 'gainward',
+      model: 'phoenix gs',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1168422',
+    },
+    {
+      brand: 'gainward',
+      model: 'phoenix',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1168129',
+    },
+    {
+      brand: 'zotac',
+      model: 'trinity',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1168398',
+    },
+    {
+      brand: 'zotac',
+      model: 'amp holo',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1170425',
+    },
+    {
+      brand: 'palit',
+      model: 'gaming pro',
+      series: '3080',
+      url: 'https://www.komplett.no/product/1168128',
+    },
+    {
+      brand: 'msi',
+      model: 'gaming x trio',
+      series: '3090',
+      url: 'https://www.komplett.no/product/1168378/',
+    },
+    {
+      brand: 'msi',
+      model: 'suprim x',
+      series: '3090',
+      url: 'https://www.komplett.no/product/1173670',
+    },
+    {
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3090',
+      url: 'https://www.komplett.no/product/1168381/',
+    },
+    {
+      brand: 'asus',
+      model: 'ekwb',
+      series: '3090',
+      url: 'https://www.komplett.no/product/1174234',
+    },
+    {
+      brand: 'asus',
+      model: 'strix oc white',
+      series: '3090',
+      url: 'https://www.komplett.no/product/1176245',
+    },
+    {
+      brand: 'asus',
+      model: 'tuf',
+      series: '3090',
+      url: 'https://www.komplett.no/product/1168359',
+    },
+    {
+      brand: 'asus',
+      model: 'strix oc',
+      series: '3090',
+      url: 'https://www.komplett.no/product/1168438',
+    },
+    {
+      brand: 'asus',
+      model: 'tuf oc',
+      series: '3090',
+      url: 'https://www.komplett.no/product/1168490',
+    },
+    {
+      brand: 'asus',
+      model: 'strix',
+      series: '3090',
+      url: 'https://www.komplett.no/product/1168437',
+    },
+
+    {
+      brand: 'gigabyte',
+      model: 'gaming oc',
+      series: '3090',
+      url: 'https://www.komplett.no/product/1168414',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'vision oc',
+      series: '3090',
+      url: 'https://www.komplett.no/product/1173940',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'aorus master',
+      series: '3090',
+      url: 'https://www.komplett.no/product/1168417',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'eagle oc',
+      series: '3090',
+      url: 'https://www.komplett.no/product/1168412',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'aorus xtreme',
+      series: '3090',
+      url: 'https://www.komplett.no/product/1168413',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'turbo',
+      series: '3090',
+      url: 'https://www.komplett.no/product/1171270',
+    },
+    {
+      brand: 'palit',
+      model: 'gamerock oc',
+      series: '3090',
+      url: 'https://www.komplett.no/product/1171795',
+    },
+    {
+      brand: 'palit',
+      model: 'gamerock',
+      series: '3090',
+      url: 'https://www.komplett.no/product/1171802',
+    },
+    {
+      brand: 'palit',
+      model: 'gaming pro',
+      series: '3090',
+      url: 'https://www.komplett.no/product/1168127',
+    },
+    {
+      brand: 'asus',
+      model: 'ekwb',
+      series: '3070',
+      url: 'https://www.komplett.no/product/1174234',
+    },
+  ],
+  name: 'komplett-no',
+};
---
+++ src/store/model/power-no.ts
@@ -0,0 +1,98 @@
+import {Store} from './store';
+
+export const PowerNO: Store = {
+  currency: 'kr.',
+  labels: {
+    inStock: {
+      container:
+        '#product-intro > div.container.old-product-page.ng-star-inserted > div > div > div.col-md-7 > div > div.product-control > div.buy-area.ng-star-inserted > div.buy-area__webshop > button',
+      text: ['Legg til handlekurv'],
+    },
+    outOfStock: {
+      container:
+        '#product-intro > div.container.old-product-page.ng-star-inserted > div > div > div.col-md-7 > div > div.product-control > div.buy-area.ng-star-inserted > div > button',
+      text: ['Ikke på lager'],
+    },
+  },
+  links: [
+    {
+      brand: 'test:brand',
+      model: 'test:model',
+      series: 'test:series',
+      url: 'https://www.power.no/data-og-tilbehoer/p-1128968/',
+    },
+    {
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url: 'https://www.power.no/data-og-tilbehoer/p-1118420/',
+    },
+    {
+      brand: 'msi',
+      model: 'gaming x trio',
+      series: '3080',
+      url: 'https://www.power.no/data-og-tilbehoer/p-1118419/',
+    },
+    {
+      brand: 'msi',
+      model: 'suprim x',
+      series: '3080',
+      url: 'https://www.power.no/data-og-tilbehoer/p-1145753/',
+    },
+    {
+      brand: 'asus',
+      model: 'tuf',
+      series: '3080',
+      url: 'https://www.power.no/data-og-tilbehoer/p-1115923/',
+    },
+    {
+      brand: 'asus',
+      model: 'strix',
+      series: '3080',
+      url: 'https://www.power.no/data-og-tilbehoer/p-1117755/',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'aorus xtreme',
+      series: '3080',
+      url: 'https://www.power.no/data-og-tilbehoer/p-1141929/',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'eagle',
+      series: '3080',
+      url: 'https://www.power.no/data-og-tilbehoer/p-1141928/',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'vision oc',
+      series: '3080',
+      url: 'https://www.power.no/data-og-tilbehoer/p-1121308/',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'aorus master',
+      series: '3080',
+      url: 'https://www.power.no/data-og-tilbehoer/p-1121307/',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'gaming oc',
+      series: '3080',
+      url: 'https://www.power.no/data-og-tilbehoer/p-1118416/',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'eagle oc',
+      series: '3080',
+      url: 'https://www.power.no/data-og-tilbehoer/p-1118415/',
+    },
+    {
+      brand: 'pny',
+      model: 'xlr8 epic x',
+      series: '3080',
+      url: 'https://www.power.no/data-og-tilbehoer/p-1128968/',
+    },
+  ],
+  name: 'power-no',
+};
