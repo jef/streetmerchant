2501f6b5cc46aa05bfe94ca238b5866e4fd60f36
Joel Calado <joelcalado@gmail.com>
2021-02-21 00:23:04 +0000

    feat(store): add PCDiga.com (#1990)

---
+++ src/store/model/pcdiga.ts
@@ -0,0 +1,170 @@
+import {Store} from './store';
+
+export const PCDiga: Store = {
+  currency: '€',
+  labels: {
+    inStock: {
+      container: '#product-addtocart-button > span',
+      text: ['Adicionar ao carrinho'],
+    },
+    outOfStock: {
+      container: 'div[id="skrey_estimate_date_product_page_wrapper"]',
+      text: ['Sem stock'],
+    },
+    maxPrice: {
+      container: '.price-container.price-final_price > .price-wrapper > span',
+      euroFormat: true,
+    },
+  },
+  links: [
+    {
+      brand: 'test:brand',
+      model: 'test:model',
+      series: 'test:series',
+      url:
+        'https://www.pcdiga.com/placa-grafica-asus-ekwb-geforce-rtx-3080-10gb-gddr6x-90yv0f60-m0nm00',
+    },
+    {
+      brand: 'asus',
+      model: 'ekwb',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-asus-ekwb-geforce-rtx-3080-10gb-gddr6x-90yv0f60-m0nm00',
+    },
+    {
+      brand: 'msi',
+      model: 'ventus 3x',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-msi-geforce-rtx-3080-ventus-3x-10g-oc-912-v389-001',
+    },
+    {
+      brand: 'msi',
+      model: 'gaming x trio',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-msi-geforce-rtx-3080-gaming-x-trio-10g-912-v389-005',
+    },
+    {
+      brand: 'zotac',
+      model: 'trinity',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-zotac-gaming-geforce-rtx-3080-10gb-gddr6x-trinity-zt-a30800d-10p',
+    },
+    {
+      brand: 'asus',
+      model: 'strix',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-asus-rog-strix-geforce-rtx-3080-10gb-gddr6x-90yv0fa0-m0nm00',
+    },
+    {
+      brand: 'asus',
+      model: 'tuf',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-asus-tuf-gaming-geforce-rtx-3080-10gb-gddr6x-90yv0fb0-m0nm00',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'aorus xtreme',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-gigabyte-geforce-rtx-3080-aorus-xtreme-10gb-gddr6x-gv-n3080aorus-x-10gd',
+    },
+    {
+      brand: 'msi',
+      model: 'suprim x',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-msi-geforce-rtx-3080-suprim-x-10g-912-v389-006',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'aorus master',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-gigabyte-geforce-rtx-3080-aorus-master-10gb-gddr6x-gvn3080am-00-10',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'aorus',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-gigabyte-rtx-3080-aorus-gaming-box-10gb-gv-n3080ixeb-10gd',
+    },
+    {
+      brand: 'pny',
+      model: 'xlr8 epic x',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-pny-geforce-rtx-3080-10gb-gddr6x-xlr8-gaming-epic-x-rgb-triple-fan-vcg308010tfxppb',
+    },
+    {
+      brand: 'pny',
+      model: 'xlr8 epic x',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-pny-geforce-rtx-3080-10gb-gddr6x-xlr8-gaming-epic-x-rgb-triple-fan-vcg308010tfxmpb',
+    },
+    {
+      brand: 'zotac',
+      model: 'trinity oc',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-zotac-gaming-geforce-rtx-3080-10gb-gddr6x-trinity-oc-zt-a30800j-10p',
+    },
+    {
+      brand: 'zotac',
+      model: 'amp holo',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-zotac-gaming-geforce-rtx-3080-10gb-gddr6x-amp-holo-zt-a30800f-10p',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'vision',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-gigabyte-geforce-rtx-3080-vision-10gb-gddr6x-oc-edition-gv-n3080vision-oc-10',
+    },
+    {
+      brand: 'asus',
+      model: 'strix',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-asus-rog-strix-geforce-rtx-3080-10gb-gddr6x-oc-edition-90yv0fa1-m0nm00',
+    },
+    {
+      brand: 'asus',
+      model: 'tuf oc',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-asus-tuf-gaming-geforce-rtx-3080-10gb-gddr6x-oc-edition-90yv0fb1-m0nm00',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'eagle oc',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-gigabyte-geforce-rtx-3080-eagle-10gb-gddr6x-oc-edition-gvn3080eo-00-10',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'gaming oc',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-gigabyte-geforce-rtx-3080-gaming-10gb-gddr6x-oc-edition-gvn3080go-00-10',
+    },
+    {
+      brand: 'asus',
+      model: 'strix oc white',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/placa-grafica-asus-rog-strix-geforce-rtx-3080-10gb-gddr6x-oc-white-edition-90yv0fa5-m0nm00',
+    },
+  ],
+  name: 'pcdiga',
+  waitUntil: 'domcontentloaded',
+};
