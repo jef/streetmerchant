633405fd82097b66de278b3a713b80532f63bb48
Beyhan Şen <33435147+BeyhanSen@users.noreply.github.com>
2021-02-28 13:18:25 +0000

    Laptopdirects IE

---
+++ src/store/model/laptopsdirect-ie.ts
@@ -0,0 +1,24 @@
+import {Store} from './store';
+
+export const LaptopsdirectIE: Store = {
+  currency: '€',
+  labels: {
+    inStock: {
+      container: 'span.atb',
+      text: ['Add to Basket »'],
+    },
+    maxPrice: {
+      container: 'div.wop',
+    },
+  },
+  links: [
+    {
+      brand: 'sony',
+      model: 'ps5 console',
+      series: 'sonyps5c',
+      url:
+        'https://www.laptopsdirect.ie/sony-playstation-5-0711719395003/version.asp',
+    },
+  ],
+  name: 'laptopsdirect-ie',
+};
