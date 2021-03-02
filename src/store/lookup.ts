8ee6815c0fb6ea129d2de600d10e6732e90ce924
Jef LeCompte <jeffreylec@gmail.com>
2021-03-02 11:52:23 -0500

    fix(amazon-uk): add label for ps5
    Fixes #2036

--- src/store/lookup.ts
+++ src/store/lookup.ts
@@ -449,15 +449,6 @@ async function lookupCardInStock(store: Store, page: Page, link: Link) {
     }
   }
 
-  // Fixme: currently causing issues
-  // Do API inventory validation in realtime (no cache) if available
-  // if (
-  // 	store.realTimeInventoryLookup !== undefined &&
-  // 	link.itemNumber !== undefined
-  // ) {
-  // 	return store.realTimeInventoryLookup(link.itemNumber);
-  // }
-
   if (store.labels.inStock) {
     const options = {
       ...baseOptions,
