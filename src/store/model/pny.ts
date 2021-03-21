9f9221850db9d83b33857c02ee53407a1d5cd7b4
Reid Kornman <kornyman14@yahoo.com>
2021-03-21 14:46:03 -0400

    fix linting issues

--- src/store/model/pny.ts
+++ src/store/model/pny.ts
@@ -23,13 +23,15 @@ export const Pny: Store = {
       brand: 'pny',
       model: 'dual fan',
       series: '3060',
-      url: 'https://www.pny.com/geforce-rtx-3060-12gb-xlr8-gaming-revel-epic-x-rgb-df',
+      url:
+        'https://www.pny.com/geforce-rtx-3060-12gb-xlr8-gaming-revel-epic-x-rgb-df',
     },
     {
       brand: 'pny',
       model: 'single fan',
       series: '3060',
-      url: 'https://www.pny.com/pny-geforce-rtx-3060-12gb-xlr8-gaming-revel-epic-x-rgb-sf',
+      url:
+        'https://www.pny.com/pny-geforce-rtx-3060-12gb-xlr8-gaming-revel-epic-x-rgb-sf',
     },
     {
       brand: 'pny',
