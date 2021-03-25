0a9ce0dd4c7681cba7681d570db44f65359d83cf
Reid Kornman <76536916+Reid-Kornman@users.noreply.github.com>
2021-03-24 23:04:22 -0400

    chore(store): add Nvidia RTX Cards (#2204)

--- src/store/model/bestbuy.ts
+++ src/store/model/bestbuy.ts
@@ -474,6 +474,20 @@ export const BestBuy: Store = {
       series: '3060ti',
       url: 'https://api.bestbuy.com/click/-/6444449/pdp',
     },
+    {
+      brand: 'pny',
+      cartUrl: 'https://api.bestbuy.com/click/-/6454319/cart',
+      model: 'dual fan',
+      series: '3060',
+      url: 'https://api.bestbuy.com/click/-/6454319/pdp',
+    },
+    {
+      brand: 'pny',
+      cartUrl: 'https://api.bestbuy.com/click/-/6454318/cart',
+      model: 'single fan',
+      series: '3060',
+      url: 'https://api.bestbuy.com/click/-/6454318/pdp',
+    },
   ],
   name: 'bestbuy',
 };
--- src/store/model/pny.ts
+++ src/store/model/pny.ts
@@ -22,6 +22,20 @@ export const Pny: Store = {
     {
       brand: 'pny',
       model: 'dual fan',
+      series: '3060',
+      url:
+        'https://www.pny.com/geforce-rtx-3060-12gb-xlr8-gaming-revel-epic-x-rgb-df',
+    },
+    {
+      brand: 'pny',
+      model: 'single fan',
+      series: '3060',
+      url:
+        'https://www.pny.com/pny-geforce-rtx-3060-12gb-xlr8-gaming-revel-epic-x-rgb-sf',
+    },
+    {
+      brand: 'pny',
+      model: 'dual fan',
       series: '3070',
       url: 'https://www.pny.com/pny-geforce-rtx-3070-8gb-df',
     },
