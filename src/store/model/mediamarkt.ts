6d4627378515af45e38729ac9b7e0ba8c4189de2
Jef LeCompte <jeffreylec@gmail.com>
2021-04-15 12:03:04 -0400

    chore: fix linting

--- src/store/model/mediamarkt.ts
+++ src/store/model/mediamarkt.ts
@@ -282,25 +282,25 @@ export const Mediamarkt: Store = {
       series: 'sonyps5c',
       url: 'https://www.mediamarkt.de/de/product/-2661938.html',
     },
-	{
+    {
       brand: 'sony',
       model: 'ps5 console',
       series: 'sonyps5c',
       url: 'https://www.mediamarkt.de/de/product/_sony-playstation%C2%AE5-hd-kamera-2715822.html',
     },
-	{
+    {
       brand: 'sony',
       model: 'ps5 console',
       series: 'sonyps5c',
       url: 'https://www.mediamarkt.de/de/product/_sony-ps5-assassins-creed-valhalla-2715828.html',
     },
-	{
+    {
       brand: 'sony',
       model: 'ps5 console',
       series: 'sonyps5c',
       url: 'https://www.mediamarkt.de/de/product/_sony-ps5-marvel-s-spider-man-miles-morales-2715827.html',
     },
-	{
+    {
       brand: 'sony',
       model: 'ps5 console',
       series: 'sonyps5c',
