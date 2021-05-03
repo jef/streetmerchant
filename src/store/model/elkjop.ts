9442527169b740d15899afc970a11f2ceb70c7e1
David Olsson <david@davido.me>
2021-05-04 00:23:27 +0200

    lintifier

--- src/store/model/elkjop.ts
+++ src/store/model/elkjop.ts
@@ -29,7 +29,7 @@ export const Elkjop: Store = {
       model: 'xlr8 epic x',
       series: '3090',
       cartUrl:
-      'https://www.elkjop.no/INTERSHOP/web/WFS/store-elkjop-Site/no_NO/-/NOK/ViewCart-AddProductByAjax?SKU=215370&Quantity=1&AddToCart=true',
+        'https://www.elkjop.no/INTERSHOP/web/WFS/store-elkjop-Site/no_NO/-/NOK/ViewCart-AddProductByAjax?SKU=215370&Quantity=1&AddToCart=true',
       url:
         'https://www.elkjop.no/product/data/pc-komponenter/grafikkort/215370/pny-rtx-3090-24gb-xlr8-gaming-epic-x-rgb-3-fan-2-7slot',
     },
