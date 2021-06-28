eb7b7215664223b633dd8125dfe25e49d464bb98
Paolo Oranges <paolo.oranges@gmail.com>
2021-06-28 17:10:45 +0200

    fix: opirata price format

--- src/store/model/opirata.ts
+++ src/store/model/opirata.ts
@@ -9,7 +9,7 @@ export const Opirata: Store = {
     },
     maxPrice: {
       container: '#app > div.wrapper-container.full > div.container.list > div.row.less-gutters > div:nth-child(2) > div > div:nth-child(2) > div > form > div.row.info-details-buy > div.col-12.col-md-6.product-price.text-right > div:nth-child(1) > div > h3',
-      euroFormat: true,
+      euroFormat: false,
     },
     outOfStock: {
       container: '#app > div.wrapper-container.full > div.container.list > div.row.less-gutters > div:nth-child(2) > div > div:nth-child(2) > div > form > div:nth-child(4) > div.col-12.text-right > button',
