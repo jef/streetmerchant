4e77c67641e85b353c1157e36cbd7c2657dbc01d
Richard Dubois <45883038+richie256@users.noreply.github.com>
2021-09-08 10:52:03 -0400

    chore(store): EBGames renamed to Gamestop (in Canada) (#2734)

--- src/store/model/gamestop-ca.ts
+++ src/store/model/gamestop-ca.ts
@@ -1,6 +1,6 @@
 import {Store} from './store';
 
-export const EbGames: Store = {
+export const GamestopCA: Store = {
   currency: '$',
   labels: {
     maxPrice: {
@@ -16,32 +16,32 @@ export const EbGames: Store = {
       brand: 'test:brand',
       model: 'test:model',
       series: 'test:series',
-      url: 'https://www.ebgames.ca/Switch/Games/727918/mario-kart-8-deluxe',
+      url: 'https://www.gamestop.ca/Switch/Games/727918/mario-kart-8-deluxe',
     },
     {
       brand: 'sony',
       model: 'ps5 console',
       series: 'sonyps5c',
-      url: 'https://www.ebgames.ca/PS5/Games/877522',
+      url: 'https://www.gamestop.ca/PS5/Games/877522',
     },
     {
       brand: 'sony',
       model: 'ps5 digital',
       series: 'sonyps5de',
-      url: 'https://www.ebgames.ca/PS5/Games/877523',
+      url: 'https://www.gamestop.ca/PS5/Games/877523',
     },
     {
       brand: 'microsoft',
       model: 'xbox series x',
       series: 'xboxsx',
-      url: 'https://www.ebgames.ca/Xbox%20Series%20X/Games/877779/xbox-series-x',
+      url: 'https://www.gamestop.ca/Xbox%20Series%20X/Games/877779/xbox-series-x',
     },
     {
       brand: 'microsoft',
       model: 'xbox series s',
       series: 'xboxss',
-      url: 'https://www.ebgames.ca/Xbox%20Series%20X/Games/877780/xbox-series-s',
+      url: 'https://www.gamestop.ca/Xbox%20Series%20X/Games/877780/xbox-series-s',
     },
   ],
-  name: 'ebgames',
+  name: 'gamestop-ca',
 };
