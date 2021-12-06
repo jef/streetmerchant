a9ef0a44e2f648430f776e3f5be4bfadb268d83a
Sierra-Romeo <63086658+Sierra-Romeo@users.noreply.github.com>
2021-12-06 19:29:53 +0000

    Update amd.ts

--- src/store/model/amd.ts
+++ src/store/model/amd.ts
@@ -83,6 +83,14 @@ export const Amd: Store = {
       series: 'rx6800xt',
       url: 'https://www.amd.com/en/direct-buy/5496921500/us',
     },
+    {
+      brand: 'amd',
+      cartUrl:
+        'https://www.amd.com/en/direct-buy/5496921400/us?add-to-cart=true',
+      model: 'amd reference',
+      series: 'rx6700xt',
+      url: 'https://www.amd.com/en/direct-buy/5496921400/us',
+    },
   ],
   name: 'amd',
 };
