f6b901f3fd3fb5a21774ad8c61106c8b9c21f42c
Techmaster13 <81822275+Mr-Tech-13@users.noreply.github.com>
2021-12-12 14:27:57 -0500

    Evga

--- src/store/model/evga.ts
+++ src/store/model/evga.ts
@@ -4,8 +4,8 @@ export const Evga: Store = {
   currency: '$',
   labels: {
     inStock: {
-      container: '.product-buy-specs',
-      text: ['add to cart'],
+      container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+      text: ['100-BR-0450-K1'],
     },
   },
   links: [
@@ -15,108 +15,246 @@ export const Evga: Store = {
       series: 'test:series',
       url: 'https://www.evga.com/products/product.aspx?pn=06G-P4-2065-KR',
     },
-    {
-      brand: 'evga',
-      model: 'xc gaming',
-      series: '3060ti',
-      url: 'https://www.evga.com/products/product.aspx?pn=08G-P5-3663-KR',
-    },
-    {
-      brand: 'evga',
-      model: 'ftw3',
-      series: '3060ti',
-      url: 'https://www.evga.com/products/product.aspx?pn=08G-P5-3665-KR',
-    },
-    {
-      brand: 'evga',
-      model: 'ftw3 ultra',
-      series: '3060ti',
-      url: 'https://www.evga.com/products/product.aspx?pn=08G-P5-3667-KR',
-    },
-    {
-      brand: 'evga',
-      model: 'xc3 black',
-      series: '3070',
-      url: 'https://www.evga.com/products/product.aspx?pn=08G-P5-3751-KR',
-    },
-    {
-      brand: 'evga',
-      model: 'xc3 ultra',
-      series: '3070',
-      url: 'https://www.evga.com/products/product.aspx?pn=08G-P5-3755-KR',
-    },
-    {
-      brand: 'evga',
-      model: 'ftw3 ultra',
-      series: '3070',
-      url: 'https://www.evga.com/products/product.aspx?pn=08G-P5-3767-KR',
-    },
-    {
-      brand: 'evga',
-      model: 'xc3 black',
-      series: '3080',
-      url: 'https://www.evga.com/products/product.aspx?pn=10G-P5-3881-KR',
-    },
-    {
-      brand: 'evga',
-      model: 'ftw3 ultra',
-      series: '3080',
-      url: 'https://www.evga.com/products/product.aspx?pn=10G-P5-3897-KR',
-    },
-    {
-      brand: 'evga',
-      model: 'ftw3 ultra hydro copper',
-      series: '3080',
-      url: 'https://www.evga.com/products/product.aspx?pn=10G-P5-3897-KR',
-    },
-    {
-      brand: 'evga',
-      model: 'ftw3',
-      series: '3080',
-      url: 'https://www.evga.com/products/product.aspx?pn=10G-P5-3895-KR',
-    },
-    {
-      brand: 'evga',
-      model: 'xc3',
-      series: '3080',
-      url: 'https://www.evga.com/products/product.aspx?pn=10G-P5-3883-KR',
-    },
-    {
-      brand: 'evga',
-      model: 'xc3 ultra',
-      series: '3080',
-      url: 'https://www.evga.com/products/product.aspx?pn=10G-P5-3885-KR',
-    },
-    {
-      brand: 'evga',
-      model: 'xc3 black',
-      series: '3090',
-      url: 'https://www.evga.com/products/product.aspx?pn=24G-P5-3971-KR',
-    },
-    {
-      brand: 'evga',
-      model: 'ftw3 ultra',
-      series: '3090',
-      url: 'https://www.evga.com/products/product.aspx?pn=24G-P5-3987-KR',
-    },
-    {
-      brand: 'evga',
-      model: 'ftw3',
-      series: '3090',
-      url: 'https://www.evga.com/products/product.aspx?pn=24G-P5-3985-KR',
-    },
-    {
-      brand: 'evga',
-      model: 'xc3',
-      series: '3090',
-      url: 'https://www.evga.com/products/product.aspx?pn=24G-P5-3973-KR',
-    },
-    {
-      brand: 'evga',
-      model: 'xc3 ultra',
-      series: '3090',
-      url: 'https://www.evga.com/products/product.aspx?pn=24G-P5-3975-KR',
-    },
+/**   {
+  brand : 'evga',
+  labels : {
+    inStock: {
+         container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+         text: ['100-BR-0450-K1'],
+             },
+            },
+         model: 'test:model',
+         series: 'test:series',
+          url: 'https://www.evga.com/products/feature.aspx',
+ },**/
+ {
+  brand : 'evga',
+  labels : {
+    inStock: {
+        container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+        text: ['08G-P5-3663-KR'],
+             },
+            },
+         model: 'xc gaming',
+         series: '3060ti',
+         url: 'https://www.evga.com/products/feature.aspx',
+},
+{
+  brand : 'evga',
+  labels : {
+    inStock: {
+        container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+        text: ['08G-P5-3663-KR'],
+             },
+            },
+         model: 'xc gaming',
+         series: '3060ti',
+         url: 'https://www.evga.com/products/feature.aspx',
+},
+{
+  brand : 'evga',
+  labels : {
+    inStock: {
+        container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+        text: ['08G-P5-3665-KR'],
+             },
+            },
+         model: 'ftw3',
+         series: '3060ti',
+         url: 'https://www.evga.com/products/feature.aspx',
+},
+{
+  brand : 'evga',
+  labels : {
+    inStock: {
+        container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+        text: ['08G-P5-3667-KR'],
+             },
+            },
+         model: 'ftw3 ultra',
+         series: '3060ti',
+         url: 'https://www.evga.com/products/feature.aspx',
+},
+{
+  brand : 'evga',
+  labels : {
+    inStock: {
+        container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+        text: ['08G-P5-3751-KR'],
+             },
+            },
+         model: 'xc3 black',
+         series: '3070',
+         url: 'https://www.evga.com/products/feature.aspx',
+},
+{
+  brand : 'evga',
+  labels : {
+    inStock: {
+        container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+        text: ['08G-P5-3755-KR'],
+             },
+            },
+         model: 'xc3 ultra',
+         series: '3070',
+         url: 'https://www.evga.com/products/feature.aspx',
+},
+{
+  brand : 'evga',
+  labels : {
+    inStock: {
+        container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+        text: ['08G-P5-3663-KR'],
+             },
+            },
+         model: 'xc gaming',
+         series: '3070',
+         url: 'https://www.evga.com/products/feature.aspx',
+},
+{
+  brand : 'evga',
+  labels : {
+    inStock: {
+        container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+        text: ['08G-P5-3767-KR'],
+             },
+            },
+         model: 'ftw3 ultra',
+         series: '3070',
+         url: 'https://www.evga.com/products/feature.aspx',
+},
+{
+  brand : 'evga',
+  labels : {
+    inStock: {
+        container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+        text: ['10G-P5-3881-KR'],
+             },
+            },
+         model: 'xc3 black',
+         series: '3080',
+         url: 'https://www.evga.com/products/feature.aspx',
+},
+{
+  brand : 'evga',
+  labels : {
+    inStock: {
+        container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+        text: ['10G-P5-3897-KR'],
+             },
+            },
+         model: 'ftw3 ultra',
+         series: '3080',
+         url: 'https://www.evga.com/products/feature.aspx',
+},
+{
+  brand : 'evga',
+  labels : {
+    inStock: {
+        container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+        text: ['10G-P5-3897-KR'],
+             },
+            },
+         model: 'ftw3 ultra hydro copper',
+         series: '3080',
+         url: 'https://www.evga.com/products/feature.aspx',
+},
+{
+  brand : 'evga',
+  labels : {
+    inStock: {
+        container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+        text: ['10G-P5-3895-KR'],
+             },
+            },
+         model: 'ftw3',
+         series: '3080',
+         url: 'https://www.evga.com/products/feature.aspx',
+},
+{
+  brand : 'evga',
+  labels : {
+    inStock: {
+        container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+        text: ['10G-P5-3883-KR'],
+             },
+            },
+         model: 'xc3',
+         series: '3080',
+         url: 'https://www.evga.com/products/feature.aspx',
+},
+{
+  brand : 'evga',
+  labels : {
+    inStock: {
+        container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+        text: ['10G-P5-3885-KR'],
+             },
+            },
+         model: 'xc3 ultra',
+         series: '3060ti',
+         url: 'https://www.evga.com/products/feature.aspx',
+},
+{
+  brand : 'evga',
+  labels : {
+    inStock: {
+        container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+        text: ['24G-P5-3971-KR'],
+             },
+            },
+         model: 'xc3 black',
+         series: '3080',
+         url: 'https://www.evga.com/products/feature.aspx',
+},
+{
+  brand : 'evga',
+  labels : {
+    inStock: {
+        container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+        text: ['24G-P5-3987-KR'],
+             },
+            },
+         model: 'ftw3 ultra',
+         series: '3090',
+         url: 'https://www.evga.com/products/feature.aspx',
+},
+{
+  brand : 'evga',
+  labels : {
+    inStock: {
+        container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+        text: ['24G-P5-3985-KR'],
+             },
+            },
+         model: 'ftw3',
+         series: '3090',
+         url: 'https://www.evga.com/products/feature.aspx',
+},
+{
+  brand : 'evga',
+  labels : {
+    inStock: {
+        container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+        text: ['24G-P5-3973-KR'],
+             },
+            },
+         model: 'xc3',
+         series: '3090',
+         url: 'https://www.evga.com/products/feature.aspx',
+},
+{
+  brand : 'evga',
+  labels : {
+    inStock: {
+        container: '#EVGAContent > div > div.main-wrapper > div.main-product',
+        text: ['24G-P5-3975-KR'],
+             },
+            },
+         model: 'xc3 ultra',
+         series: '3090',
+         url: 'https://www.evga.com/products/feature.aspx',
+},
   ],
   name: 'evga',
 };
