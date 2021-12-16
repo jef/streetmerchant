7f6e068af853cedce1d86af53eb91a56ff364e46
Techmaster13 <81822275+Mr-Tech-13@users.noreply.github.com>
2021-12-15 19:54:19 -0500

    Add files via upload

--- src/store/model/evga.ts
+++ src/store/model/evga.ts
@@ -4,8 +4,8 @@ export const Evga: Store = {
   currency: '$',
   labels: {
     inStock: {
-      container: '#EVGAContent > div > div.main-wrapper > div.main-product',
-      text: ['100-BR-0450-K1'],
+      container: '',
+      text: [''],
     },
   },
   links: [
