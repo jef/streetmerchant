a146ea351db46e21553990f625a90dbdbd72f395
Destructor <54399962+OscarleBG@users.noreply.github.com>
2022-02-14 21:02:51 +0100

    chore(ldlc): add 3050 (#2948)

--- src/store/model/ldlc.ts
+++ src/store/model/ldlc.ts
@@ -42,6 +42,12 @@ export const Ldlc: Store = {
       url: 'https://www.ldlc.com/fr-be/fiche/PB00369299.html',
     },
     {
+      brand: 'msi',
+      model: 'ventus 2X (LHR)',
+      series: '3050',
+      url: 'https://www.ldlc.com/fiche/PB00480344.html',
+    },
+    {
       brand: 'gigabyte',
       model: 'gaming oc',
       series: '3080',
@@ -64,6 +70,12 @@ export const Ldlc: Store = {
       model: 'twin x2',
       series: '3080',
       url: 'https://www.ldlc.com/fr-be/fiche/PB00368690.html',
+    },
+    {
+      brand: 'inno3d',
+      model: 'twin x2',
+      series: '3050',
+      url: 'https://www.ldlc.com/fiche/PB00481183.html',
     },
     {
       brand: 'palit',
