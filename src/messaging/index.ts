33ff8d68c88846224863dbaa3bce36bcd53329c6
Mr-Tech-13 <maxxshearey@hotmail.com>
2022-10-02 11:23:45 -0400

    add

--- src/messaging/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './captcha';
-export * from './notification';
-
-export type DMPayloadType = 'text' | 'image';
-
-export interface DMPayload {
-  content: string; // for image type, content is local file path
-  type: DMPayloadType;
-}
