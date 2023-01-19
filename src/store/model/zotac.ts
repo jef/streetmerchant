491986dad74678e55272433c2ee566df88a69795
Jef LeCompte <jeffreylec@gmail.com>
2023-01-19 01:14:24 -0800

    Update zotac.ts

--- src/store/model/zotac.ts
+++ src/store/model/zotac.ts
@@ -89,12 +89,13 @@ export const Zotac: Store = {
       model: 'trinity',
       series: '4090',
       url: 'https://www.zotac.com/product/graphics_card/zotac-gaming-geforce-rtx-4090-trinity',
-    }, {
+    },
+    {
       brand: 'zotac',
       model: 'amp extreme airo',
       series: '4090',
       url: 'https://www.zotac.com/product/graphics_card/zotac-gaming-geforce-rtx-4090-amp-extreme-airo',
-    }, 
+    },
     {
       brand: 'zotac',
       model: 'trinity oc',
